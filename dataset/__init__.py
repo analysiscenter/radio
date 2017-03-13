05c50025713e4ee309437399149abb5db7a68644
Roman Kh <rhudor@gmail.com>
2017-03-13 18:49:45 +0300

    Moved dataset to repo analysiscenter/dataset

--- dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-""" Dataset module implements Dataset, DatasetIndex, Preprocess and Batch classes"""
-
-from .batch import Batch, ArrayBatch, DataFrameBatch
-from .dataset import Dataset
-from .fulldataset import FullDataset
-from .dsindex import DatasetIndex, FilesIndex, DirectoriesIndex
-from .preprocess import Preprocessing, action
