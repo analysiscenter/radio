600b80494750e597c8abf90eb91eedca6702e182
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-05-04 20:59:40 +0300

    added ct_masked_batch.py

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .preprocessing import *
+from .preprocessing.dataset_import import ds
---
+++ preprocessing/dataset_import.py
@@ -0,0 +1,7 @@
+"""Import of dataset."""
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from dataset import *
+import dataset as ds
