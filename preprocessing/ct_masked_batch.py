702f4765ebeef315bb41622b6f0ab01a6f672a7e
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-05-15 17:22:17 +0300

    changed jit to njit

--- preprocessing/ct_masked_batch.py
+++ preprocessing/ct_masked_batch.py
@@ -18,7 +18,7 @@ from .dataset_import import action
 from .dataset_import import inbatch_parallel
 from .dataset_import import any_action_failed
 
-@jit('float64[:, :, :](float64[:, :, :], int32[:, :], int32[:])', nogil=True)
+@njit('float64[:, :, :](float64[:, :, :], int32[:, :], int32[:])', nogil=True)
 def get_nodules_jit(data, positions, size):
     """Fetch nodules from array by array of starting positions.
 
