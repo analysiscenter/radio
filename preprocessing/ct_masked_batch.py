3a394ce12212969181089e22eab9ccaffd778afb
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-05-18 13:37:10 +0300

    Removed extra blank line

--- preprocessing/ct_masked_batch.py
+++ preprocessing/ct_masked_batch.py
@@ -460,7 +460,6 @@ class CTImagesMaskedBatch(CTImagesBatch):
         nodules_batch.mask = mask
         return nodules_batch
 
-
     def get_axial_slice(self, patient_pos, height):
         """Get tuple of slices (data slice, mask slice).
 
