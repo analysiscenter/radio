614940b0f2d0fa92c089cf6f539d9b3800a5ab96
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-08-08 20:00:30 +0300

    Left only one type of conv_block;

--- models/keras_resnet.py
+++ models/keras_resnet.py
@@ -53,35 +53,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     return x
 
 
-def conv_block_I(input_tensor, kernel_size, filters, stage, block, strides=(2, 2, 2)):
-    """ A block that has a conv layer at shortcut. """
-    filters1, filters2, filters3 = filters
-
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
-
-    x = Conv3D(filters1, (1, 1, 1), strides=strides,
-               name=conv_name_base + '2a')(input_tensor)
-    x = BatchNormalization(axis=4, name=bn_name_base + '2a')(x)
-    x = Activation('relu')(x)
-
-    x = Conv3D(filters2, kernel_size, padding='same',
-               name=conv_name_base + '2b')(x)
-    x = BatchNormalization(axis=4, name=bn_name_base + '2b')(x)
-    x = Activation('relu')(x)
-
-    x = Conv3D(filters3, (1, 1, 1), name=conv_name_base + '2c')(x)
-    x = BatchNormalization(axis=4, name=bn_name_base + '2c')(x)
-
-    shortcut = Conv3D(filters3, (1, 1, 1), strides=strides,
-                      name=conv_name_base + '1')(input_tensor)
-    shortcut = BatchNormalization(axis=4, name=bn_name_base + '1')(shortcut)
-
-    x = layers.add([x, shortcut])
-    x = Activation('relu')(x)
-    return x
-
-def conv_block_II(input_tensor, kernel_size, filters, stage, block, strides=(2, 2, 2)):
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2, 2)):
     """ A block that has a conv layer at shortcut. """
     filters1, filters2, filters3 = filters
 
@@ -90,24 +62,38 @@ def conv_block_II(input_tensor, kernel_size, filters, stage, block, strides=(2, 
 
     x = BatchNormalization(axis=4, name=bn_name_base + '2a')(input_tensor)
     x = Activation('relu')(x)
-    x = Conv3D(filters1, (1, 1, 1), strides=strides,
-               name=conv_name_base + '2a', use_bias=False, kernel_initializer='glorot_normal')(x)
+    x = Conv3D(filters1, (1, 1, 1),
+               strides=strides,
+               name=conv_name_base + '2a',
+               use_bias=False,
+               kernel_initializer='glorot_normal')(x)
 
     x = BatchNormalization(axis=4, name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
-    x = Conv3D(filters2, kernel_size, padding='same',
-               name=conv_name_base + '2b', use_bias=False, kernel_initializer='glorot_normal')(x)
+    x = Conv3D(filters2,
+               kernel_size,
+               padding='same',
+               name=conv_name_base + '2b',
+               use_bias=False,
+               kernel_initializer='glorot_normal')(x)
 
     x = BatchNormalization(axis=4, name=bn_name_base + '2c')(x)
-    x = Conv3D(filters3, (1, 1, 1), name=conv_name_base + '2c', use_bias=False, kernel_initializer='glorot_normal')(x)
+    x = Conv3D(filters3, (1, 1, 1),
+               name=conv_name_base + '2c',
+               use_bias=False,
+               kernel_initializer='glorot_normal')(x)
 
     shortcut = BatchNormalization(axis=4, name=bn_name_base + '1')(input_tensor)
-    shortcut = Conv3D(filters3, (1, 1, 1), strides=strides,
-                      name=conv_name_base + '1', use_bias=False, kernel_initializer='glorot_normal')(shortcut)
+    shortcut = Conv3D(filters3, (1, 1, 1),
+                      strides=strides,
+                      name=conv_name_base + '1',
+                      use_bias=False,
+                      kernel_initializer='glorot_normal')(shortcut)
 
     x = layers.add([x, shortcut])
     x = Activation('relu')(x)
     return x
+
 
 def build_resnet_I(input_tensor, dropout_rate):
     x = conv_block_I(x, 3, [16, 16, 64], stage=2, block='a', strides=(1, 1, 1))
