6ad4684e95cc99740417d125ec1493f4acca6234
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-08-10 14:31:54 +0300

    Added tf_model module with description of tensorflow base model;

---
+++ models/tf_model.py
@@ -0,0 +1,248 @@
+import os
+import sys
+import glob
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from .base_model import BaseModel
+
+
+ACTIVATIONS = {'relu': tf.nn.relu,
+               'selu': tf.identity,
+               'linear': tf.identity,
+               'sigmoid': tf.nn.sigmoid,
+               'softmax': tf.nn.softmax,
+               'tanh': tf.nn.tanh}
+
+
+def get_activation(activation_str):
+    """ Get tf activation function by its name. """
+    if activation_str is None:
+        return lambda x: x
+    return ACTIVATIONS[activation_str]
+
+
+def get_shape(input_tensor):
+    """ Return full shape of the input tensor represented by tuple of ints.
+
+    Args:
+    - input_tensor: tf.Variable, input_tensor.
+
+    Return:
+    - shape of input_tensor, tuple(int);
+    """
+    return input_tensor.get_shape().as_list()
+
+
+def model(method):
+    """ Wrap method of TFModel to apply all ops in context of model's graph and scope. """
+    @wraps(method)
+    def wrapped(self, *args, **kwargs):
+        with self.graph.as_default():
+            with tf.variable_scope(self.name):
+                result = method(self, *args, **kwargs)
+        return result
+    return wrapped
+
+
+class TFModel(BaseModel):
+
+    def __new__(cls, name, *args, **kwargs):
+        """ Add necessary attributes to object of tensorflow model. """
+        instance = super(TFModel, cls).__new__(cls)
+
+        instance.name = name
+        instance.phase = True
+
+        graph = tf.Graph()
+        instance._graph = graph
+        with graph.as_default():
+            with tf.variable_scope(name):
+                instance._phase_placeholder = tf.placeholder(tf.bool)
+        return instance
+
+    def __init__(self, name, *args, **kwargs):
+        self.phase = True
+
+    @property
+    def graph(self):
+        """ Get tensorflow graph object. """
+        return self._graph
+
+    @property
+    def session(self):
+        """ Get tensorflow session. """
+        return self._session
+
+    @property
+    def learning_phase(self):
+        """ Get learning phase represented by tf.placeholder(tf.bool). """
+        return self._phase_placeholder
+
+    def get_number_of_trainable_vars(self):
+        with self.graph.as_default():
+            arr = np.asarray([np.prod(get_shape(v)) for v in tf.trainable_variables()])
+        return np.sum(arr)
+
+    @staticmethod
+    def flatten(input_tensor):
+        """ Flatten input tensor.
+
+        Args:
+        - input_tensor: tf.Variable, input tensor;
+
+        Return:
+        - tf.Variable, flattened input tensor;
+        """
+        return tf.contrib.layers.flatten(input_tensor)
+
+    @staticmethod
+    def num_channels(input_tensor):
+        """ Return channels dimension of the input tensor.
+
+        Args:
+        - input_tensor: tf.Variable, input tensor.
+
+        Return:
+        - number of channels, int;
+        """
+        return input_tensor.get_shape().as_list()[-1]
+
+    @staticmethod
+    def batch_size(input_tensor):
+        """ Return batch size of the input tensor represented by first dimension.
+
+        Args:
+        - input_tensor: tf.Variable, input_tensor.
+
+        Return:
+        - number of items in the batch, int;
+        """
+        return input_tensor.get_shape().as_list()[0]
+
+    @staticmethod
+    def get_shape(input_tensor):
+        """ Return full shape of the input tensor represented by tuple of ints.
+
+        Args:
+        - input_tensor: tf.Variable, input_tensor.
+
+        Return:
+        - shape of input_tensor, tuple(int);
+        """
+        return input_tensor.get_shape().as_list()
+
+    @staticmethod
+    def identity(input_tensor, name):
+        """ Create an alias with given name for input_tensor.
+
+        Args:
+        - input_tensor: tf.Variable, input tensor;
+        - name: str, name of alias;
+
+        Return:
+        - alias of input tensor, tf.Variable;
+        """
+        return tf.identity(input_tensor, name=name)
+
+    def dense(self, input_tensor, units, name, activation='linear'):
+        """ Wrap input tensor with dense layer.
+
+        Args:
+        - input_tensor: tf.Variable, input tensor;
+        - units: int, number of units in the output tensor;
+        - name: str, name of the dense layer's scope;
+        - activation: str, activation to put after convolution;
+
+        Return:
+        - output tensor, tf.Variable;
+        """
+        with tf.variable_scope('Dense', name):
+            init_w = tf.truncated_normal(shape=(self.num_channels(input_tensor),
+                                                units), dtype=tf.float32)
+            w = tf.Variable(init_w)
+            b = tf.Variable(tf.random_uniform(shape=(units, ), dtype=tf.float32))
+
+            output_tensor = tf.matmul(input_tensor, w) + b
+            output_tensor = get_activation(activation)(output_tensor)
+        return output_tensor
+
+    @staticmethod
+    def maxpool3d(input_tensor, pool_size, strides, name, padding='same'):
+        """ Apply maxpooling3d operation to the input_tensor. """
+        with tf.variable_scope(name):
+            out_tensor = tf.layers.max_pooling3d(input_tensor,
+                                                 pool_size=pool_size,
+                                                 strides=strides,
+                                                 padding=padding,
+                                                 name='maxpool3d')
+        return out_tensor
+
+    @staticmethod
+    def averagepool3d(input_tensor, pool_size, strides, name, padding='same'):
+        """ Apply averagepool3d operation to the input_tensor. """
+        with tf.variable_scope(name):
+            out_tensor = tf.layers.average_pooling3d(input_tensor,
+                                                     pool_size=pool_size,
+                                                     strides=strides,
+                                                     padding='same',
+                                                     name='average_pool3d')
+        return out_tensor
+
+    @staticmethod
+    def global_averagepool3d(input_tensor, name):
+        with tf.variable_scope(name):
+            output_layer = tf.reduce_mean(input_tensor, axis=(1, 2, 3))
+        return output_layer
+
+    @staticmethod
+    def conv3d(input_tensor, filters, kernel_size, name,
+               strides=(1, 1, 1), padding='same', activation=None, use_bias=True):
+        activation_fn = get_activation(activation)
+        with tf.variable_scope(name):
+            output_tensor = tf.layers.conv3d(input_tensor, filters=filters,
+                                          kernel_size=kernel_size,
+                                          strides=strides,
+                                          use_bias=use_bias,
+                                          name='conv3d', padding=padding)
+
+            output_tensor = activation_fn(output_tensor)
+        return output_tensor
+
+    def bn_conv3d(self, input_tensor, filters, kernel_size, name,
+                  strides=(1, 1, 1), padding='same', activation=None, use_bias=False):
+        activation_fn = get_activation(activation)
+        with tf.variable_scope(name):
+            output_tensor = tf.layers.conv3d(input_tensor, filters=filters,
+                                             kernel_size=kernel_size,
+                                             strides=strides,
+                                             use_bias=use_bias,
+                                             name='conv3d', padding=padding)
+
+            output_tensor = tf.layers.batch_normalization(output_tensor, axis=-1,
+                                                          training=self.learning_phase)
+            output_tensor = activation_fn(output_tensor)
+        return output_tensor
+
+    def dropout(self, input_tensor, rate=0.3):
+        """ Add dropout layer with given dropout rate to the input tensor. """
+        return tf.layers.dropout(input_tensor, rate=rate, training=self.learning_phase)
+
+    def train_on_batch(self, x, y, callbacks=None, **kwargs):
+        raise NotImplementedError
+
+    def test_on_batch(self, x, y, callbacks=None, **kwargs):
+        raise NotImplementedError
+
+    def predict_on_batch(self, x, **kwargs):
+        raise NotImplementedError
+
+    def save(self, dir_path, *args, **kwargs):
+        raise NotImplementedError
+
+    def load(self, dir_path, *args, **kwargs):
+        raise NotImplementedError
+
+    def compile(self, *args, **kwargs):
+        raise NotImplementedError
