7b5f4b1ef693e018562cba2801e343d4baff2eaf
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-08-10 19:28:09 +0300

    Commented import from models_batch

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,10 @@
 """ Module of nn-models for classification/segmentation of
 lung-cancer on CT-scans
 """
-from .models_batch import CTImagesModels
+# from .models_batch import CTImagesModels
+from .dense_net import DenseNet
+from .keras_unet import KerasUnet
+from .keras_resnet import KerasResNet
+from .keras_vgg16 import KerasVGG16
+from .keras_model import KerasModel
+from .tf_model import TFModel
