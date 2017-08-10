d4159bc37c5916ba2d8b3c2230a58cf309871181
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-08-10 19:26:42 +0300

    Added prints in dense net; added initial max polling layer;

--- models/dense_net.py
+++ models/dense_net.py
@@ -1,8 +1,9 @@
 import numpy as np
 import tensorflow as tf
+from collections import namedtuple
 
 from .tf_model import TFModel
-from .tf_model import get_activation
+from .tf_model import get_activation, get_shape
 from .tf_model import model
 
 
@@ -49,27 +50,38 @@ class DenseNet(TFModel):
         input_tensor = tf.placeholder(shape=(None, 32, 64, 64, 1), dtype=tf.float32, name='x')
         y_true = tf.placeholder(shape=(None, 1), dtype=tf.float32, name='y_true')
 
-        x = self.conv3d(input_tensor, filters=64, strides=(1, 1, 1), kernel_size=(5, 5, 5),
+        x = self.conv3d(input_tensor, filters=16, strides=(1, 1, 1), kernel_size=(5, 5, 5),
                         padding='same', name='initial_convolution')
+
+        x = self.maxpool3d(x, pool_size=(3, 3, 3), strides=(1, 2, 2),
+                           padding='same', name='initial_pooling')
+
+        print(get_shape(x))
 
         x = self.dense_block(x, filters=32, block_size=6, name='dense_block_1')
         x = self.transition_layer(x, filters=32, name='transition_layer_1')
+
         print(get_shape(x))
 
         x = self.dense_block(x, filters=32, block_size=12, name='dense_block_2')
         x = self.transition_layer(x, filters=32, name='transition_layer_2')
+
         print(get_shape(x))
 
         x = self.dense_block(x, filters=32, block_size=32, name='dense_block_3')
         x = self.transition_layer(x, filters=32, name='transition_layer_3')
+
         print(get_shape(x))
 
         x = self.dense_block(x, filters=32, block_size=32, name='dense_block_4')
         x = self.transition_layer(x, filters=32, name='transition_layer_4')
+
         print(get_shape(x))
 
         y_pred = self.global_averagepool3d(x, name='global_average_pool3d')
 
-        y_pred = self.dense(y_pred, units=1, name='dense32_1', activation='linear')
+        print(get_shape(y_pred))
+
+        y_pred = self.dense(y_pred, units=1, name='dense32_1', activation='sigmoid')
         y_pred = tf.identity(y_pred, name='y_pred')
         return {'x': input_tensor, 'y_true': y_true, 'y_pred': y_pred}
