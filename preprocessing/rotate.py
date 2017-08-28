67876245985fa36e33f0da2123bcf5d4633f02e8
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-08-28 22:14:14 +0300

    Added rotate_3D jit decorated function

---
+++ preprocessing/rotate.py
@@ -0,0 +1,23 @@
+""" Module with jit-compilated functions for rotation operation of 3D scans. """
+
+import numpy as np
+from numba import jit
+import scipy.ndimage
+
+
+@jit(nogil=True)
+def rotate_3D(image, degree, axes=(1, 2)):
+    """ Rotate 3D image along two axes.
+
+    Args:
+    - image: ndarray(l, k, m), 3D image;
+    - degree: float, degree of rotation;
+    - axes: tuple(int, int), axes that specify rotation plane;
+
+    Returns:
+    - ndarray(l, k, m), 3D rotated image;
+
+    *Note: zero padding automatically added after rotation;
+    """
+    rotated_image = scipy.ndimage.interpolation.rotate(data, degree, axes)
+    return rotated_image
