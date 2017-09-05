55dba8ea054743683dda8b1041638857844654b4
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-09-05 17:43:11 +0300

    Removed nogil decorator for make_central_crop function;

--- preprocessing/crop.py
+++ preprocessing/crop.py
@@ -3,7 +3,6 @@ import numpy as np
 from numba import njit
 
 
-@njit(nogil=True)
 def make_central_crop(image, crop_size):
     """ Make crop from from center of 3D image;
     This function returns crop from center of the source image(image argument)
