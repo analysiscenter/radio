dc0643f51c81d85c2e2337253e4d415e83536e5c
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-09-05 12:52:00 +0300

    Removed compile method from KerasResNet model;

--- models/keras_resnet.py
+++ models/keras_resnet.py
@@ -94,9 +94,6 @@ class KerasResNet(KerasModel):
         self.input_tensor = Input(shape=(32, 64, 64, 1))
         super().__init__(name, dropout_rate=dropout_rate, input_tensor=self.input_tensor, **kwargs)
 
-    def compile(self, optimizer='adam', loss='binary_crossentropy'):
-        super().compile(optimizer=optimizer, loss=loss)
-
     @staticmethod
     def build_resnet(input_tensor, dropout_rate):
         """ Build resnet model implemented in keras.
