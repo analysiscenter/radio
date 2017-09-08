164fa1054d1a3a8268aecf95d4ff7abb73a34415
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-09-08 20:25:25 +0300

    Splitted vgg16 model into blocks

--- models/keras_vgg16.py
+++ models/keras_vgg16.py
@@ -11,6 +11,59 @@ from keras.layers import Dense, BatchNormalization
 from .keras_model import KerasModel
 
 
+def reduction_block_I(input_tensor, filters, scope, padding='same'):
+    with tf.variable_scope(scope):
+        conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                       activation='relu', padding=padding,
+                       name='conv_1')(input_tensor)
+        conv1 = BatchNormalization(axis=4)(conv1)
+
+        conv2 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                       activaton='relu', padding=padding,
+                       name='conv2')(conv1)
+        conv2 = BatchNormalization(axis=4)(conv2)
+
+        max_pool = MaxPooling3D((2, 2, 2), strides=(2, 2, 2),
+                                name='max_pool_3D')(conv2)
+    return max_pool
+
+
+def reduction_block_II(input_tensor, filters, scope, padding='same'):
+    with tf.variable_scope(scope):
+        conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                       activation='relu', padding=padding,
+                       name='conv1')(input_tensor)
+        conv1 = BatchNormalization(axis=4)(conv1)
+
+        conv2 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                       activaton='relu', padding=padding,
+                       name='conv2')(conv1)
+        conv2 = BatchNormalization(axis=4)(conv2)
+
+        conv3 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                       activaton='relu', padding=padding,
+                       name='conv3')(conv2)
+        conv3 = BatchNormalization(axis=4)(conv3)
+
+        max_pool = MaxPooling3D((2, 2, 2), strides=(2, 2, 2),
+                                name='max_pool_3D')(conv3)
+    return max_pool
+
+
+def classification_block(input_tensor, filters=(512, 256), scope='ClassificationBlock'):
+    with tf.variable_scope(scope):
+        filters_1, filters_2 = filters
+
+        layer = Flatten(name='flatten')(input_tensor)
+        layer = Dense(filters_1, activation='relu', name='fc1')(layer)
+        layer = BatchNormalization(axis=-1)(layer)
+        layer = Dropout(0.3)(layer)
+        layer = Dense(filters_2, activation='relu', name='fc2')(layer)
+        layer = BatchNormalization(axis=-1)(layer)
+        layer = Dropout(0.2)(layer)
+    return layer
+
+
 class KerasVGG16(KerasModel):
     """ KerasVGG16 model for 3D scans implemented in keras. """
     def __init__(self, name, **kwargs):
@@ -18,75 +71,20 @@ class KerasVGG16(KerasModel):
 
     @staticmethod
     def build_vgg16():
-        """ Build vgg16 model for 3D scans implemented in keras. """
+        input_tensor = Input(shape=(32, 64, 64, 64, 1))
+        block_A = reduction_block_I(img_input, 32, scope='Block_A')
+        block_B = reduction_block_I(block_A, 64, scope='Block_B')
+        block_C = reduction_block_II(block_B, 128, scope='Block_C')
+        block_D = reduction_block_II(block_C, 256, scope='Block_D')
+        block_E = reduction_block_II(block_D, 256, scope='Block_E')
 
-        img_input = Input(shape=(32, 64, 64, 1))
-        layer = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block1_conv1')(img_input)
-        layer = BatchNormalization(axis=4)(layer)
+        block_F = classification_block(block_E, (512, 256),
+                                       scope='ClassificationBlock')
 
-        layer = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block1_conv2')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), name='block1_pool')(layer)
+        output_tensor = Dense(1, activation='sigmoid',
+                              name='predictions')(block_F)
 
-        # Block 2
-        layer = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block2_conv1')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=64, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block2_conv2')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), name='block2_pool')(layer)
-
-        # Block 3
-        layer = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block3_conv1')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block3_conv2')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=128, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block3_conv3')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), name='block3_pool')(layer)
-
-        # Block 4
-        layer = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block4_conv1')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block4_conv2')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block4_conv3')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), name='block4_pool')(layer)
-
-        # Block 5
-        layer = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block5_conv1')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block5_conv2')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = Conv3D(filters=256, kernel_size=(3, 3, 3), activation='relu',
-                       padding='same', name='block5_conv3')(layer)
-        layer = BatchNormalization(axis=4)(layer)
-        layer = MaxPooling3D((2, 2, 2), strides=(2, 2, 2), name='block5_pool')(layer)
-
-        # Classification block
-        layer = Flatten(name='flatten')(layer)
-        layer = Dense(512, activation='relu', name='fc1')(layer)
-        layer = BatchNormalization(axis=-1)(layer)
-        layer = Dropout(0.3)(layer)
-        layer = Dense(256, activation='relu', name='fc2')(layer)
-        layer = BatchNormalization(axis=-1)(layer)
-        layer = Dropout(0.2)(layer)
-
-        layer = Dense(1, activation='sigmoid', name='predictions')(layer)
-
-        model = Model(img_input, layer, name='vgg16')
+        model = Model(input_tensor, output_tensor, name='vgg16')
         return model
 
     @classmethod
