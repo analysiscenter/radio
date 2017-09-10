9ceaa957a7f92d12a655b74de3ce3e6604f2ebe7
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-09-10 22:09:41 +0300

    Added docstrings for blocks; put blocks inside class of the model;

--- models/keras_vgg16.py
+++ models/keras_vgg16.py
@@ -11,63 +11,114 @@ from keras.layers import Dense, BatchNormalization
 from .keras_model import KerasModel
 
 
-def reduction_block_I(input_tensor, filters, scope, padding='same'):
-    with tf.variable_scope(scope):
-        conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
-                       activation='relu', padding=padding,
-                       name='conv_1')(input_tensor)
-        conv1 = BatchNormalization(axis=4)(conv1)
-
-        conv2 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
-                       activaton='relu', padding=padding,
-                       name='conv2')(conv1)
-        conv2 = BatchNormalization(axis=4)(conv2)
-
-        max_pool = MaxPooling3D((2, 2, 2), strides=(2, 2, 2),
-                                name='max_pool_3D')(conv2)
-    return max_pool
-
-
-def reduction_block_II(input_tensor, filters, scope, padding='same'):
-    with tf.variable_scope(scope):
-        conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
-                       activation='relu', padding=padding,
-                       name='conv1')(input_tensor)
-        conv1 = BatchNormalization(axis=4)(conv1)
-
-        conv2 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
-                       activaton='relu', padding=padding,
-                       name='conv2')(conv1)
-        conv2 = BatchNormalization(axis=4)(conv2)
-
-        conv3 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
-                       activaton='relu', padding=padding,
-                       name='conv3')(conv2)
-        conv3 = BatchNormalization(axis=4)(conv3)
-
-        max_pool = MaxPooling3D((2, 2, 2), strides=(2, 2, 2),
-                                name='max_pool_3D')(conv3)
-    return max_pool
-
-
-def classification_block(input_tensor, filters=(512, 256), scope='ClassificationBlock'):
-    with tf.variable_scope(scope):
-        filters_1, filters_2 = filters
-
-        layer = Flatten(name='flatten')(input_tensor)
-        layer = Dense(filters_1, activation='relu', name='fc1')(layer)
-        layer = BatchNormalization(axis=-1)(layer)
-        layer = Dropout(0.3)(layer)
-        layer = Dense(filters_2, activation='relu', name='fc2')(layer)
-        layer = BatchNormalization(axis=-1)(layer)
-        layer = Dropout(0.2)(layer)
-    return layer
-
-
 class KerasVGG16(KerasModel):
     """ KerasVGG16 model for 3D scans implemented in keras. """
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
+
+    def reduction_block_I(input_tensor, filters, scope, padding='same'):
+        """ Reduction block of type I for VGG16 architecture.
+
+        Applyes 3D-convolution with kernel size (3, 3, 3), (1, 1, 1) strides
+        and 'relu' activation, after performs batch noramlization, then
+        again 3D-convolution with kernel size (3, 3, 3),
+        strides (1, 1, 1) and 'relu' activation,  that batch normalization;
+        After all applyes 3D maxpooling operation with strides (2, 2, 2)
+        and pooling size (2, 2, 2).
+
+        Args:
+        - input_tensor: keras tensor, input tensor;
+        - filters: int, number of filters in 3D-convolutional layers;
+        - scope: str, name of the scope;
+        - padding: str, padding mode can be 'same' or 'valid';
+
+        Returns:
+        - output tensor, keras tensor;
+        """
+        with tf.variable_scope(scope):
+            conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                           activation='relu', padding=padding,
+                           name='conv_1')(input_tensor)
+            conv1 = BatchNormalization(axis=4)(conv1)
+
+            conv2 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                           activaton='relu', padding=padding,
+                           name='conv2')(conv1)
+            conv2 = BatchNormalization(axis=4)(conv2)
+
+            max_pool = MaxPooling3D((2, 2, 2), strides=(2, 2, 2),
+                                    name='max_pool_3D')(conv2)
+        return max_pool
+
+
+    def reduction_block_II(input_tensor, filters, scope, padding='same'):
+        """ Reduction block of type II for VGG16 architecture.
+
+        Applyes 3D-convolution with kernel size (3, 3, 3), strides (1, 1, 1)
+        and 'relu' activation, after that preform batch noramlization,
+        repeates this combo three times;
+        Finally, adds 3D maxpooling layer with
+        strides (2, 2, 2) and pooling size (2, 2, 2).
+
+        Args:
+        - input_tensor: keras tensor, input tensor;
+        - filters: int, number of filters in 3D-convolutional layers;
+        - scope: str, name of the scope;
+        - padding: str, padding mode can be 'same' or 'valid';
+
+        Returns:
+        - output tensor, keras tensor;
+        """
+        with tf.variable_scope(scope):
+            conv1 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                           activation='relu', padding=padding,
+                           name='conv1')(input_tensor)
+            conv1 = BatchNormalization(axis=4)(conv1)
+
+            conv2 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                           activaton='relu', padding=padding,
+                           name='conv2')(conv1)
+            conv2 = BatchNormalization(axis=4)(conv2)
+
+            conv3 = Conv3D(filters=filters, kernel_size=(3, 3, 3),
+                           activaton='relu', padding=padding,
+                           name='conv3')(conv2)
+            conv3 = BatchNormalization(axis=4)(conv3)
+
+            max_pool = MaxPooling3D((2, 2, 2), strides=(2, 2, 2),
+                                    name='max_pool_3D')(conv3)
+        return max_pool
+
+
+    def classification_block(input_tensor, units=(512, 256),
+                             dropout_rate=0.35, scope='ClassificationBlock'):
+        """ Classification block of VGG16 architecture.
+
+        This block consists of flatten operation applied to input_tensor.
+        Then there is two fully connected layers with 'relu' activation,
+        batch normalization and dropout layers. This block should be put
+        in the end of the model.
+
+        Args:
+        - input_tensor: keras tensor, input tensor;
+        - units: tuple(int, int), number of units in first and second dense layers;
+        - dropoout_rate: float, probability of dropout;
+        - scope: str, name of scope;
+
+        Returns:
+        - output tensor, keras tensor;
+        """
+        with tf.variable_scope(scope):
+            units_1, units_2 = units
+
+            layer = Flatten(name='flatten')(input_tensor)
+            layer = Dense(units_1, activation='relu', name='fc1')(layer)
+            layer = BatchNormalization(axis=-1)(layer)
+            layer = Dropout(dropout_rate)(layer)
+            layer = Dense(units_2, activation='relu', name='fc2')(layer)
+            layer = BatchNormalization(axis=-1)(layer)
+            layer = Dropout(dropout_rate)(layer)
+        return layer
 
     @staticmethod
     def build_vgg16():
