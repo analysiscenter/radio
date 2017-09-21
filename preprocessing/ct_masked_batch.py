2613119b3f7f281a81d005561cf1189b3b6153a5
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-09-21 13:12:33 +0300

    Added global pylint disable=too-many-locals

--- preprocessing/ct_masked_batch.py
+++ preprocessing/ct_masked_batch.py
@@ -1,5 +1,6 @@
 # pylint: disable=no-member
 # pylint: disable=too-many-public-methods
+# pylint: disable=too-many-locals
 """Contains class CTImagesMaskedBatch for storing masked Ct-scans."""
 import logging
 
