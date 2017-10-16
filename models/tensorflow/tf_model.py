2f94325ce6236e64d8ef661eb4bbddb7256f299b
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-16 18:13:22 +0300

    Added bn_dilated_conv3d

--- models/tensorflow/tf_model.py
+++ models/tensorflow/tf_model.py
@@ -422,3 +422,40 @@ class TFModel(BaseModel):
                                                           training=self.learning_phase)
             output_tensor = activation(output_tensor)
         return output_tensor
+
+    def bn_dilated_conv3d(self, input_tensor, filters,
+                          kernel_size, name, activation=tf.nn.relu,
+                          dilation=(1, 1, 1), padding='same'):
+        """ Apply 3D convolution operation with batch normalization to input tensor.
+
+        Args:
+        - input_tensor: tf.Variable, input tensor;
+        - filters: int, number of filters in the ouput tensor;
+        - kernel_size: tuple(int, int, int), size of kernel
+          of 3D convolution operation along 3 dimensions;
+        - name: str, name of the layer that will be used as an argument of tf.variable_scope;
+        - dilation: tuple(int, int, int), size of dilation along 3 dimensions;
+        - padding: str, padding mode, can be 'same' or 'valid';
+        - activation: tensorflow activation function that will be applied to
+        output tensor;
+
+        Returns:
+        - tf.Variable, output tensor;
+        """
+
+        in_filters = input_tensor.get_shape().as_list()[-1]
+        with tf.variable_scope(name):
+            w = tf.get_variable(name='W', shape=(*kernel_size, in_filters, filters),
+                                initializer=tf.contrib.layers.xavier_initializer())
+
+            output_tensor = tf.nn.convolution(input_tensor, w,
+                                              padding=padding.upper(),
+                                              strides=(1, 1, 1),
+                                              dilation_rate=dilation)
+
+            output_tensor = tf.layers.batch_normalization(output_tensor,
+                                                          axis=4, training=self.learning_phase)
+
+            output_tensor = activation(output_tensor)
+
+        return output_tensor
