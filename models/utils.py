c6e698ec5a7d3c88ca306de519714ff7cbcd4dbf
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-18 18:28:24 +0300

    Added utils.py

---
+++ models/utils.py
@@ -0,0 +1,68 @@
+import numpy as np
+from numba import njit
+
+
+def nodules_info_to_rzyx(nodules, scale=True):
+    """ Transform data contained in nodules_info array to rzyx format. """
+    if scale:
+        _centers = (nodules.nodule_center - nodules.origin) / nodules.spacing
+        _rads = (nodules.nodule_size / nodules.spacing)
+    return np.hstack([np.expand_dims(_rads.max(axis=1), axis=1), _centers])
+
+
+@njit(cache=True)
+def sphere_overlap(nodule_true, nodule_pred):
+    """ Two nodules overlap volume normalized by total volume of second one.
+
+    Args:
+    - nodule_true: ndarray with information about true nodule;
+    nodule_true[1:] - [z,y,x] coordinates of true nodule's center,
+    nodule_true[0] - radius of true nodule;
+    - nodule_pred: ndarray with information about predicted nodule;
+    nodule_pred[1:] - [z,y,x] coordinates of predicted nodule's center,
+    nodule_pred[0] - radius of predicted nodule;
+
+    Returns:
+    - float, normalized overlap volume.
+    """
+    r1, r2 = nodule_true[3], nodule_pred[3]
+    pos1, pos2 = nodule_true[:3], nodule_pred[:3]
+
+    pos1_area = 4. / 3. * np.pi * r1 ** 3
+    pos2_area = 4. / 3. * np.pi * r2 ** 3
+
+    d = np.sum((pos1 - pos2) ** 2) ** 0.5
+
+    if d >= r1 + r2:
+        return 0
+    elif r1 > d + r2:
+        if r1 > 5 * r2:
+            return 0
+        else:
+            return 1
+    elif r2 > d + r:
+        return 1
+
+    volume = (np.pi * (r1 + r2 - d) ** 2 * (d ** 2
+                 + r1 * (2 * d - 3 * r1)
+                 + r2 * (2 * d - 3 * r2)
+                 + 6 * r1 * r2)) / (12 * d)
+    return volume / pos2_area
+
+
+@njit(cache=True, parallel=True)
+def nodules_sets_overlap_jit(nodules_true, nodules_pred):
+    """ Compute overlap matrix for two sets of nodules. """
+    num_pred = nodules_pred.shape[0]
+    num_true = nodules_true.shape[1]
+
+    r_true, r_pred = nodules_true[:, 0], nodules_pred[:, 0]
+
+    overlap_matrix = np.zeros(shape=(num_true, num_pred), dtype=np.float)
+    for i in range(num_pred):
+        for j in range(num_true):
+            overlap_volume = sphere_overlap(nodules_true[j, 1:],
+                                            nodules_pred[i, 1:])
+            overlap_matrix[j, i] = overlap_volume
+
+    return overlap_matrix
