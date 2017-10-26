5ea9216e70a0647925bbf762daecb35b26459e82
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-26 16:54:53 +0300

    Removed method get_number_of_trainable_vars cause it is already in TFModel;

--- models/tensorflow/tf_model.py
+++ models/tensorflow/tf_model.py
@@ -14,9 +14,3 @@ from ...dataset.dataset.models.tf import TFModel
 
 class TFModel3D(TFModel):
     """ Base class for all tensorflow models. """
-
-    def get_number_of_trainable_vars(self):
-        """ Get number of trainable variable in graph associated with current model. """
-        with self.graph.as_default():
-            arr = np.asarray([np.prod(self.get_shape(v)) for v in tf.trainable_variables()])
-        return np.sum(arr)
