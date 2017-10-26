0176e0810e284980e3aba822368f6adc8aeacdfa
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-26 13:14:28 +0300

    Changed import: TFModel => TFModel3D

--- models/tensorflow/architectures/dilated_vnet.py
+++ models/tensorflow/architectures/dilated_vnet.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import tensorflow as tf
-from ..tf_model import TFModel
+from ..tf_model import TFModel3D
 from ..layers import conv3d, bn_conv3d, bn_dilated_conv3d
 from ..utils import repeat_tensor
 
