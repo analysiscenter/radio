960f960eacfb857cec0c662f3e77533b0e9c98f4
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-26 14:37:19 +0300

    Removed self.name = ... from TFResNet;

--- models/tensorflow/architectures/resnet.py
+++ models/tensorflow/architectures/resnet.py
@@ -17,7 +17,6 @@ class TFResNet(TFModel3D):
 
     def __init__(self, name, num_targets=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = self.get_from_config('name')
         self.num_targets = self.get_from_config('num_targets', 1)
 
     def identity_block(self, input_tensor, kernel_size, filters, name):
