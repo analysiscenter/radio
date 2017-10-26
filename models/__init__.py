3c56e7128b1e93d0c7f9d3afbde8c6a8a81e4719
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-26 14:16:18 +0300

    Corrected imports in __init__.py of models.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
 """ Module of nn-models for classification/segmentation of
 lung-cancer on CT-scans
 """
-from .models_batch import CTImagesModels
+from .models_batch import CTImagesModels as CTImagesModelsBatch
 from .tensorflow.architectures import TFDenseNet
 from .tensorflow.architectures import TFResNet
 from .tensorflow.architectures import TFDilatedVnet
 from .keras.architectures import KerasUnet
 from .keras.architectures import KerasResNet50
 from .keras.architectures import KerasVGG16
-from .tensorflow import TFModel
-from .keras import KerasModel
+from .tensorflow import TFModel3D
+from .keras import KerasModel as KerasModel3D
