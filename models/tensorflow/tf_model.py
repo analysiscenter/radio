77bbfa9544bed1164488e9a00efaa0393487fcf0
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-26 16:43:05 +0300

    Removed all static methods from TFModel3D cause they are already in TFModel;

--- models/tensorflow/tf_model.py
+++ models/tensorflow/tf_model.py
@@ -20,39 +20,3 @@ class TFModel3D(TFModel):
         with self.graph.as_default():
             arr = np.asarray([np.prod(self.get_shape(v)) for v in tf.trainable_variables()])
         return np.sum(arr)
-
-    @staticmethod
-    def num_channels(input_tensor):
-        """ Return channels dimension of the input tensor.
-
-        Args:
-        - input_tensor: tf.Variable, input tensor.
-
-        Return:
-        - number of channels, int;
-        """
-        return input_tensor.get_shape().as_list()[-1]
-
-    @staticmethod
-    def batch_size(input_tensor):
-        """ Return batch size of the input tensor represented by first dimension.
-
-        Args:
-        - input_tensor: tf.Variable, input_tensor.
-
-        Return:
-        - number of items in the batch, int;
-        """
-        return input_tensor.get_shape().as_list()[0]
-
-    @staticmethod
-    def get_shape(input_tensor):
-        """ Return full shape of the input tensor represented by tuple of ints.
-
-        Args:
-        - input_tensor: tf.Variable, input_tensor;
-
-        Return:
-        - shape of input_tensor, tuple(int);
-        """
-        return input_tensor.get_shape().as_list()
