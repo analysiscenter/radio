517e6c5bd9cf04ba22772ec16a6ebd582ce6ff94
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-27 19:30:46 +0300

    Added metrics in TFModel3D

--- models/tensorflow/tf_model.py
+++ models/tensorflow/tf_model.py
@@ -7,6 +7,7 @@ import os
 import functools
 import json
 import numpy as np
+from IPython.display import clear_output
 import tensorflow as tf
 
 from ...dataset.dataset.models.tf import TFModel
@@ -14,4 +15,38 @@ from ...dataset.dataset.models.tf import TFModel
 
 class TFModel3D(TFModel):
     """ Base class for all tensorflow models. """
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+        self._show_metrics = self.get_from_config('show_metrics', False)
+        _metrics = self.get_from_config('metrics', [])
+        if not isinstance(_metrics, (list, tuple)):
+            _metrics = [_metrics]
+        self._metrics = _metrics
+        self._metrics_values = []
+
+    @property
+    def train_metrics(self):
+        """ Return pandas DataFrame containing train metrics. """
+        return pd.DataFrame(self._metrics_values)
+
+    def compute_metrics(self, y_true, y_pred):
+        """ Compute all metrics on train. """
+        return {metric.__name__: metric(y_true, y_pred)
+                for metric in self._metrics}
+
+    def train(self, fetches=None, feed_dict=None):
+        """ Train model with data provided. """
+        if fetches is None:
+            _fetches = tuple()
+        elif not isinstance(fetches, (tuple, list)):
+            _fetches = (fetches, )
+        _fetches = ('y_pred', *fetches)
+        train_output = super().train(_fetches, feed_dict)
+
+        self._metrics_values.append(self.compute_metrics(feed_dict['y'],
+                                                         train_output[0]))
+
+        if self._show_metrics:
+            print(self._metrics_values.iloc[-1, :])
+            clear_output(wait=True)
+        return train_output
