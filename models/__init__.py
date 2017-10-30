7e36bfb4dbd511f12ccc213bd92fddba4a3b894b
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-30 18:59:54 +0300

    Changed import KerasUnet => KerasVnet;

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ from .models_batch import CTImagesModels as CTImagesModelsBatch
 from .tensorflow.architectures import TFDenseNet
 from .tensorflow.architectures import TFResNet
 from .tensorflow.architectures import TFDilatedVnet
-from .keras.architectures import KerasUnet
+from .keras.architectures import KerasVnet
 from .keras.architectures import KerasResNet50
 from .keras.architectures import KerasVGG16
 from .tensorflow import TFModel3D
