6a6369bdf8bca538fcdd5c31ccd8aef0e9ae6a43
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-30 19:05:07 +0300

    Minor changes in docstring;

--- models/keras/architectures/keras_resnet.py
+++ models/keras/architectures/keras_resnet.py
@@ -53,9 +53,9 @@ class KerasResNet50(KerasModel):
         If kernel_size is (3, 3, 3) then
         schematically this block can be represented like this:
         =======================================================================
-                                   input_tensor _________
-                                        ||                \
-                                        \/                 \
+                                   input_tensor ------------|
+                                        ||                  |
+                                        \/                  |
                             Conv3D{1x1x1}[1:1:1](filters1)  |
                                         ||                  |
                                         \/                  |
