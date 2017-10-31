f24513bc51cfbeb7acc571c813e7ff614cab6c75
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-31 22:31:52 +0300

    Added docstring;

--- models/keras/architectures/__init__.py
+++ models/keras/architectures/__init__.py
@@ -1,3 +1,4 @@
+""" Contains neural network architectures for lung cancer detection implemented in keras. """
 from .keras_resnet import KerasResNet50
 from .keras_vnet import KerasVnet
 from .keras_vgg16 import KerasVGG16
