71f00a7feff544c2952f48157249841be9b6791d
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-31 22:30:50 +0300

    Added docstring;

--- models/keras/__init__.py
+++ models/keras/__init__.py
@@ -1,2 +1,3 @@
+""" Contains losses, usefull utils and ANN architectures implemented via keras. """
 from .keras_model import KerasModel
 from .architectures import KerasVGG16, KerasVnet, KerasResNet50
