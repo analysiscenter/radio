4f98c90b1d6f7a32e9b6feecd211e2d6aefc6a57
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-31 21:57:24 +0300

    Removed import CTImagesModels

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 """ Module of nn-models for classification/segmentation of
 lung-cancer on CT-scans
 """
-from .models_batch import CTImagesModels as CTImagesModelsBatch
 from .tensorflow.architectures import TFDenseNet
 from .tensorflow.architectures import TFResNet
 from .tensorflow.architectures import TFDilatedVnet
