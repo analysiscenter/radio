e1a87827e0d7b7b34696d40288b1d45b95f31ae7
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-10-31 22:41:07 +0300

    Added docstring;

--- models/tensorflow/architectures/__init__.py
+++ models/tensorflow/architectures/__init__.py
@@ -1,3 +1,4 @@
+""" Contains neural network architectures for lung cancer detection implemented in tensorflow. """
 from .dense_net import TFDenseNet
 from .resnet import TFResNet
 from .dilated_vnet import TFDilatedVnet
