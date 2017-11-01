d8dc787fc73600ba0aa11e505a1fe8104ffbcdd4
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-11-01 13:38:06 +0300

    Enhanced docstrings;

--- models/keras/losses.py
+++ models/keras/losses.py
@@ -11,6 +11,8 @@ def dice_loss(y_true, y_pred, smooth=1e-6):
         tensor containing target mask.
     y_pred : keras tensor
         tensor containing predicted mask.
+    smooth : float
+        small real value used for avoiding division by zero error.
 
     Returns
     -------
@@ -33,6 +35,12 @@ def tiversky_loss(y_true, y_pred, alpha=0.3, beta=0.7, smooth=1e-10):
         tensor containing target mask.
     y_pred : keras tensor
         tensor containing predicted mask.
+    alpha : float
+        real value, weight of '0' class.
+    beta : float
+        real value, weight of '1' class.
+    smooth : float
+        small real value used for avoiding division by zero error.
 
     Returns
     -------
@@ -56,6 +64,8 @@ def jaccard_coef_logloss(y_true, y_pred, smooth=1e-10):
         tensor containing target mask.
     y_pred : keras tensor
         tensor containing predicted mask.
+    smooth : float
+        small real value used for avoiding division by zero error.
 
     Returns
     -------
@@ -68,4 +78,4 @@ def jaccard_coef_logloss(y_true, y_pred, smooth=1e-10):
     falsepos = K.sum(y_pred) - truepos
     falseneg = K.sum(y_true) - truepos
     jaccard = (truepos + smooth) / (smooth + truepos + falseneg + falsepos)
-    return -K.log(jaccard)
+    return -K.log(jaccard + smooth)
