a25bf81c05aef9a684c0c0568b1fdda0377fa6dd
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-11-03 18:04:27 +0300

    Added import of dataset in __init__.py;

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=import-error
 # pylint: disable=wildcard-import
 """3d ct-scans preprocessing module with dataset submodule."""
+import dataset
 from .preprocessing import *
 from .models import *
-
