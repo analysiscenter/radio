e97eb39fd6a5f13e7871ac93a8297e68c704fcf8
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-11-08 14:14:46 +0300

    changed import order in __init__.py; removed wildcard import of models

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@
 # pylint: disable=wildcard-import
 """3d ct-scans preprocessing module with dataset submodule."""
 import importlib
-dataset = importlib.import_module('.dataset', __package__)
 from .preprocessing import *
-from .models import *
 from .pipelines import *
+dataset = importlib.import_module('.dataset', __package__)
