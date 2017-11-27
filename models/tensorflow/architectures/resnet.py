a619c5b14d54ce7a1fa3b10fdcd16aed2d4a1dfd
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-11-27 19:09:57 +0300

    Remove old-styled ResNet implementation Add implementation based on simple configuration of ResNet model from dataset

--- models/tensorflow/architectures/resnet.py
+++ models/tensorflow/architectures/resnet.py
@@ -4,204 +4,31 @@
 # pylint: disable=anomalous-backslash-in-string
 """ Contains TFResNet model class. """
 
-import tensorflow as tf
-from ..tf_model import TFModelCT
-from ..layers import bn_conv3d
+
+from ...dataset.models.tf import ResNet
 
 
-class TFResNoduleNet(TFModelCT):
-    """ This class implements 3D ResNoduleNet architecture via tensorflow.
+class NoduleResNet(ResNet):
+    """ Implementation of custom DenseNet architecture for lung cancer detection.
 
     Full description of similar 2D model architecture can be downloaded from here:
     https://arxiv.org/pdf/1512.03385v1.pdf
-
-    Attributes
-    ----------
-    config : dict
-        config dictionary from dataset pipeline
-        see configuring model section of dataset module
-        https://analysiscenter.github.io/dataset/intro/models.html.
-    name : str
-        name of the model, can be specified in config dict.
-    units : tuple(int, int) or tuple(int) or tuple()
-        number of units in two final dense layers before tensor with predicitons,
-        can be specified in config dict. Can be int or None.
-    num_targets : int
-        size of tensor with predicitons.
-    dropout_rate : float
-        probability of dropout, can be specified in config dict.
-
-    NOTE
-    ----
-    This class is descendant of TFModel class from dataset.models.*.
-    This implementation requires the input tensor having shape=(batch_size, 32, 64, 64, 1).
     """
 
-    def __init__(self, *args, **kwargs):
-        self.config = kwargs.get('config', {})
-        self.num_targets = self.get_from_config('num_targets', 1)
-        self.dropout_rate = self.get_from_config('dropout_rate', 0.35)
+    @classmethod
+    def default_config(cls):
+        """ Sepcification of custom block parameters. """
+        config = ResNet.default_config()
 
-        _units = self.get_from_config('units', (256, 128))
-        if _units is None:
-            self.units = ()
-        elif isinstance(_units, int):
-            self.units = (_units, )
+        input_config = dict(layout='cnap', filters=16, kernel_size=7,
+                            pool_size=3, pool_strides=(1, 2, 2))
+        config['input_block'].update(input_config)
 
-        super().__init__(*args, **kwargs)
+        config['body']['num_blocks'] = [3, 4, 6, 3]
+        filters = 16   # number of filters in the first block
+        config['body']['filters'] = 2 ** np.arange(len(config['body']['num_blocks'])) * filters \
+                                    * config['body']['block']['width_factor']
 
-    def identity_block(self, input_tensor, kernel_size, filters, name):
-        """ The identity block is the block that has no conv layer at shortcut.
-
-        First of all, 3D-convolution with (1, 1, 1) kernel size, batch normalization
-        and relu activation is applied. Then the result flows into
-        3D-convolution with (3, 3, 3) kernel size, batch normalization and
-        relu activation. Finally, the result of previous convolution goes
-        into 3D-convolution with (1, 1, 1) kernel size, batch normalization
-        without activation and its output is summed with the input tensor
-        and 'relu' activation is applied.
-        Argument `filters` should be tuple(int, int, int) and specifies
-        number of filters in first, second and third convolution correspondingly.
-        Number of filters in third convolution must be the same as in the input
-        tensor.
-
-        Parameters
-        ----------
-        input_tensor : tf.Tensor
-            input tensor.
-        kernel_size : tuple(int, int, int)
-            size of kernel along three dimensions for middle convolution operation in block.
-        filters : tuple(int, int, int)
-            number for filters in first, second and third 3D-convolution operations.
-        name : str
-            name of block that will be used as argument of of tf.variable_scope.
-
-        Returns
-        -------
-        tf.Tensor
-            output tensor.
-        """
-        filters1, filters2, filters3 = filters
-
-        with tf.variable_scope(name):
-            x = bn_conv3d(input_tensor, filters1, (1, 1, 1),
-                          name='bn_conv_a', padding='same',
-                          activation=tf.nn.relu,
-                          is_training=self.is_training)
-
-            x = bn_conv3d(x, filters2, kernel_size,
-                          name='bn_conv_b', padding='same',
-                          activation=tf.nn.relu,
-                          is_training=self.is_training)
-
-            x = bn_conv3d(x, filters3, (1, 1, 1),
-                          name='bn_conv_c', padding='same',
-                          activation=tf.identity,
-                          is_training=self.is_training)
-
-            output_tensor = tf.add(x, input_tensor)
-            output_tensor = tf.nn.relu(output_tensor)
-        return output_tensor
-
-    def conv_block(self, input_tensor, kernel_size, filters, name, strides=(2, 2, 2)):
-        """ Convolutional block that has a conv layer as shortcut.
-
-        First of all, 3D-convolution with (1, 1, 1) kernel size, (2, 2, 2)-strides,
-        batch normalization and 'relu' activation is applied. Then the result
-        flows into 3D-convolution with (3, 3, 3) kernel size, batch normalization
-        and 'relu' activation. Finally, the result of previous convolution goes
-        into 3D-convolution with (1, 1, 1) kernel size, batch normalization
-        without activation and its output is summed with the result
-        of 3D-convolution with kernel_size=(1, 1, 1), strides=(2, 2, 2) and
-        batch normalization of input_tensor. After that 'relu' activation
-        is applied to the result of 'add' operation.
-        Argument `filters` should be tuple(int, int, int) and specifies
-        number of filters in first, second and third convolution correspondingly.
-
-        Parameters
-        ----------
-        input_tensor : tf.Tensor
-            input tensor.
-        kernel_size : tuple(int, int, int)
-            size of kernel along three dimensions for middle convolution operation in block.
-        filters : tuple(int, int, int)
-            number of filters in first, second, and third 3D-convolution operations.
-
-        Returns
-        -------
-        tf.Tensor
-            output tensor.
-        """
-        filters1, filters2, filters3 = filters
-
-        with tf.variable_scope(name):
-            x = bn_conv3d(input_tensor, filters1, (1, 1, 1),
-                          name='bn_conv_a', padding='same',
-                          activation=tf.nn.relu, strides=strides,
-                          is_training=self.is_training)
-
-            x = bn_conv3d(x, filters2, kernel_size,
-                          name='bn_conv_b', padding='same',
-                          activation=tf.nn.relu, is_training=self.is_training)
-
-            x = bn_conv3d(x, filters3, (1, 1, 1),
-                          name='bn_conv_c', padding='same',
-                          activation=tf.identity,
-                          is_training=self.is_training)
-
-            shortcut = bn_conv3d(input_tensor, filters3, (1, 1, 1),
-                                 strides=strides, name='bn_conv3d_shortcut',
-                                 padding='same', activation=tf.identity,
-                                 is_training=self.is_training)
-
-            output_tensor = tf.add(x, shortcut)
-            output_tensor = tf.nn.relu(output_tensor)
-        return output_tensor
-
-    def _build(self, *args, **kwargs):
-        """ Build NoduleResNet model implemented via tensorflow. """
-        input_tensor = tf.placeholder(shape=(None, 32, 64, 64, 1), dtype=tf.float32, name='x')
-        y_true = tf.placeholder(shape=(None, self.num_targets), dtype=tf.float32, name='targets')
-
-        x = bn_conv3d(input_tensor, filters=32, kernel_size=(5, 3, 3),
-                      strides=(1, 2, 2), name='initial_conv', padding='same',
-                      is_training=self.is_training)
-
-        x = self.conv_block(x, (3, 3, 3), [16, 16, 64], name='conv_1A', strides=(1, 1, 1))
-        x = self.identity_block(x, (3, 3, 3), [16, 16, 64], name='identity_1B')
-        x = self.identity_block(x, (3, 3, 3), [16, 16, 64], name='identity_1C')
-        x = tf.layers.dropout(x, rate=self.dropout_rate, training=self.is_training)
-
-        x = self.conv_block(x, (3, 3, 3), [32, 32, 128], name='conv_2A')
-        x = self.identity_block(x, (3, 3, 3), [32, 32, 128], name='identity_2B')
-        x = self.identity_block(x, (3, 3, 3), [32, 32, 128], name='identity_2C')
-        x = self.identity_block(x, (3, 3, 3), [32, 32, 128], name='identity_2D')
-        x = tf.layers.dropout(x, rate=self.dropout_rate, training=self.is_training)
-
-        x = self.conv_block(x, (3, 3, 3), [64, 64, 256], name='conv_3A')
-        x = self.identity_block(x, (3, 3, 3), [64, 64, 256], name='identity_3B')
-        x = self.identity_block(x, (3, 3, 3), [64, 64, 256], name='identity_3C')
-        x = self.identity_block(x, (3, 3, 3), [64, 64, 256], name='identity_3D')
-        x = self.identity_block(x, (3, 3, 3), [64, 64, 256], name='identity_3E')
-        x = self.identity_block(x, (3, 3, 3), [64, 64, 256], name='identity_3F')
-        x = tf.layers.dropout(x, rate=self.dropout_rate, training=self.is_training)
-
-        x = self.conv_block(x, (3, 3, 3), [128, 128, 512], name='conv_4A')
-        x = self.identity_block(x, (3, 3, 3), [128, 128, 512], name='identity_4B')
-        x = self.identity_block(x, (3, 3, 3), [128, 128, 512], name='identity_4C')
-
-        z = tf.contrib.layers.flatten(x)
-
-        for i, units in enumerate(self.units):
-            z = tf.layers.dense(z, units=units, name='dense_' + str(i))
-            z = tf.layers.batch_normalization(z, axis=-1, training=self.is_training)
-            z = tf.nn.relu(z)
-
-        z = tf.layers.dense(z, units=self.num_targets, name='dense_' + str(self.num_targets))
-        z = tf.nn.sigmoid(z)
-
-        y_pred = tf.identity(z, name='predictions')
-
-        self.store_to_attr('y', y_true)
-        self.store_to_attr('x', input_tensor)
-        self.store_to_attr('y_pred', y_pred)
+        config['head'].update(dict(layout='Vdfna', activation=tf.nn.sigmoid))
+        config['body']['block']['bottleneck'] = True
+        return config
---
+++ models/tensorflow/architectures/unet.py
@@ -0,0 +1,308 @@
+""" Implementation of custom unet for lung cancer detection. """
+
+import numpy as np
+import tensorflow as tf
+
+from ....dataset.models.tf.layers import conv_block
+from ....dataset.models.tf import TFModel
+
+
+def dilated_branches(input_tensor, filters, kernel_size, dilation_rate, name,
+                     activation=tf.nn.relu, padding='same', is_training=True):
+    """ Convolutional block with parallel branches having different dilation rate.
+
+    Parameters
+    ----------
+    input_tensor : tf.Tensor
+        input tensor
+    filters : tuple(int, ...)
+        number of filters corresponding to branches with different dilation rate.
+    kernel_size : tuple(int, ...)
+        size of convolutional kernel corresponding to branches
+        with different dilation rate. Kernel size is considered
+        to be the same along [zyx] dimensions.
+    dilation_rate : tuple(int, ...)
+        dilation rate for convolutional branches. Dilation rate is considered
+        to be the same along [zyx] dimensions.
+    activation : tensorflow activation function
+    padding : str
+        padding to use in convolution operation. Can be 'same' or 'valid'.
+    is_training : bool or bool tensor
+        indicator of training or prediction mode.
+    name : str
+        name of the block that will be used as argument of tf.variable_scope.
+
+    Returns
+    -------
+    tf.Tensor
+    """
+
+    if not all(isinstance(arg, (tuple, list)) for arg in (filters, kernel_size, dilation_rate)):
+        raise ValueError("Arguments 'filters', 'kernel_size', 'dilation_rate' "
+                         +"must be tuples or lists")
+
+    branches = []
+    with tf.variable_scope(name):
+        for f, k, d in zip(filters, kernel_size, dilation_rate):
+
+            if not isinstance(k, (tuple, list)):
+                _ksize = [k] * 3
+            else:
+                _ksize = k
+
+            if not isinstance(d, (tuple, list)):
+                _drate = [d] * 3
+            else:
+                _drate = d
+
+            b = bn_dilated_conv3d(input_tensor, f, _ksize, padding=padding,
+                                  is_training=is_training, activation=activation,
+                                  dilation=_drate, name='conv3d_rate_{}'.format(_drate[0]))
+            branches.append(b)
+        output_tensor = tf.concat(branches, axis=4)
+
+    return output_tensor
+
+
+class TFNoduleUNet(TFModel):
+    """ UNet
+
+    **Configuration**
+
+    inputs : dict
+        dict with keys 'images' and 'masks' (see :meth:`._make_inputs`)
+
+    body : dict
+        num_blocks : int
+            number of downsampling/upsampling blocks (default=4)
+
+        filters : list of int
+            number of filters in each block (default=[128, 256, 512, 1024])
+
+    head : dict
+        num_classes : int
+            number of semantic classes
+    """
+    @classmethod
+    def default_config(cls):
+        config = TFModel.default_config()
+
+        filters = 32
+
+        config['input_block'].update({})
+        config['body']['upsampling_kernel'] = 3
+        config['body']['num_blocks'] = 4
+        config['body']['filters'] = 2 ** np.arange(config['body']['num_blocks']) * filters * 2
+        config['body']['dilation_rate'] = [1, 2]
+        config['body']['dilation_share'] = [0.5, 0.5]
+        config['body']['upsampling_mode'] = 'deconv'
+        return config
+
+    def build_config(self, names=None):
+        config = super().build_config(names)
+        config['head']['num_classes'] = self.num_classes('targets')
+        return config
+
+    @classmethod
+    def input_block(cls, inputs, **kwargs):
+        """ Input block of neural network. """
+        return tf.identity(inputs)
+
+    @classmethod
+    def upsampling_block(cls, inputs, filters, name, **kwargs):
+        """ 3x3 convolution and 2x2 transposed convolution or upsampling
+
+        Each of two 3x3x3 convolutions contains several branches with
+        different dilation rate.
+
+        Parameters
+        ----------
+        inputs : tuple(tf.Tensor, tf.Tensor)
+            two input tensors
+        filters : int
+            number of output filters
+        name : str
+            scope name
+
+        Returns
+        -------
+        tf.Tensor
+        """
+        config = cls.fill_params('body', **kwargs)
+        kernel = cls.pop('upsampling_kernel', config)
+        is_training = cls.pop('is_training', config)
+
+        mode = cls.pop('upsampling_mode', config) # Added
+        dilation_rate = cls.pop('dilation_rate', config) # Added
+        dilation_share = np.asarray(cls.pop('dilation_share', config))
+        dilation_share /= dilation_share.sum()
+        _filters = np.rint(filters * dilation_share).astype(np.int).tolist()
+
+        if kwargs.get('data_format') == 'channels_last':
+            repeat_times = (1, 2, 2, 2, 1)
+            axis = -1
+        else:
+            repeat_times = (1, 1, 2, 2, 2)
+            axis = 1
+
+        with tf.variable_scope(name):
+            x, skip = inputs
+
+            if mode == 'deconv':
+                x = tf.layers.conv3d_transpose(x, filters, kernel, name='upsample',
+                                               strides=2, activation=tf.identity, use_bias=False)
+            elif mode == 'repeat':
+                x = repeat_tensor(x, repeat_times)
+
+            x = cls.crop(x, skip, data_format=kwargs.get('data_format'))
+            x = tf.concat((skip, x), axis=axis)
+
+            x = dilated_branches(x, _filters, (3, 3, 3), dilation_rate,
+                                 name='conv_I', is_training=is_training)
+
+            x = dilated_branches(x, _filters, (3, 3, 3), dilation_rate,
+                                 name='conv_II', is_training=is_training)
+
+        logger.debug("Output of {0} block: {1}".format(name, cls.get_shape(x)))
+        return x
+
+    @classmethod
+    def downsampling_block(cls, inputs, filters, name, **kwargs):
+        """ Two 3x3x3 convolutions and 2x2x2 max pooling with stride 2.
+
+        Each of two 3x3x3 convolutions contains several branches with
+        different dilation rate.
+
+        Parameters
+        ----------
+        inputs : tf.Tensor
+            input tensor
+        filters : int
+            number of output filters
+        name : str
+            scope name
+
+        Returns
+        -------
+        tf.Tensor
+        """
+        config = cls.fill_params('body', **kwargs)
+        is_training = cls.pop('is_training', config)
+
+        dilation_rate = cls.pop('dilation_rate', config)
+
+        dilation_share = np.asarray(cls.pop('dilation_share', config))
+        dilation_share /= dilation_share.sum()
+        _filters = np.rint(filters * dilation_share).astype(np.int).tolist()
+        with tf.variable_scope(name):
+
+                x = dilated_branches(inputs, _filters, (3, 3, 3), dilation_rate,
+                                     name='conv_I', is_training=is_training)
+
+                x = dilated_branches(x, _filters, (3, 3, 3), dilation_rate,
+                                     name='conv_II', is_training=is_training)
+
+                downsampled_x = tf.layers.max_pooling3d(x, pool_size=(2, 2, 2),
+                                                        strides=(2, 2, 2), padding='same',
+                                                        name='max_pool3d')
+
+        logger.debug("Output of {0} block: {1}".format(name, cls.get_shape(downsampled_x)))
+        return x, downsampled_x
+
+    @classmethod
+    def central_block(cls, inputs, filters, name, **kwargs):
+        """ Block that situated between encoder and decoder branches.
+
+        Block consists of 1x1x1 followed by 3x3x3. Note that 3x3x3 convolution
+        can contain several branches with different dilation rate.
+
+        Parameters
+        ----------
+        inputs : tf.Tensor
+             input tensor
+        filters : int
+            number of output filters
+        name : str
+            scope name
+
+        Returns
+        -------
+        tf.Tensor
+        """
+        config = cls.fill_params('body', **kwargs)
+        is_training = cls.pop('is_training', config)
+
+        dilation_rate = cls.pop('dilation_rate', config)
+
+        dilation_share = np.asarray(cls.pop('dilation_share', config))
+        dilation_share /= dilation_share.sum()
+        _filters = np.rint(filters * dilation_share).astype(np.int).tolist()
+
+        with tf.variable_scope(name):
+            x = bn_conv3d(inputs, filters, (1, 1, 1), padding='same',
+                          name='conv3D1x1x1', is_training=is_training)
+
+            x = dilated_branches(x, _filters, (3, 3, 3), dilation_rate,
+                                 name='conv3D_dilated', is_training=is_training)
+        logger.debug("Output of {0} block: {1}".format(name, cls.get_shape(x)))
+        return x
+
+    @classmethod
+    def body(cls, inputs, name='body', **kwargs):
+        """ Base layers.
+
+        Parameters
+        ----------
+        inputs : tf.Tensor
+            input tensor
+        filters : tuple of int
+            number of filters in downsampling blocks
+        name : str
+            scope name
+
+        Returns
+        -------
+        tf.Tensor
+        """
+        kwargs = cls.fill_params('body', **kwargs)
+        filters = kwargs.pop('filters')
+
+        with tf.variable_scope(name):
+            x = inputs
+            encoder_outputs = []
+            for i, ifilters in enumerate(filters[:-1]):
+                y, x = cls.downsampling_block(x, ifilters, name='downsampling-'+str(i), **kwargs)
+                encoder_outputs.append(y)
+
+            x = cls.central_block(x, filters[-1], name='central_block', **kwargs)
+
+            for i, ifilters in enumerate(filters[:-1][::-1]):
+                x = cls.upsampling_block((x, encoder_outputs[-i-1]), ifilters//2,
+                                         name='upsampling-'+str(i), **kwargs)
+        return x
+
+    @classmethod
+    def head(cls, inputs, num_classes, name='head', **kwargs):
+        """ Conv block followed by 1x1 convolution.
+
+        Parameters
+        ----------
+        inputs : tf.Tensor
+            input tensor
+        num_classes : int
+            number of classes (and number of filters in the last 1x1 convolution)
+        name : str
+            scope name
+
+        Returns
+        -------
+        tf.Tensor
+        """
+        kwargs = cls.fill_params('head', **kwargs)
+        with tf.variable_scope(name):
+            x = conv_block(inputs, name='conv', **kwargs)
+            x = conv_block(inputs, name='last', **{**kwargs, **dict(filters=num_classes,
+                                                                    kernel_size=1,
+                                                                    activation=tf.nn.sigmoid,
+                                                                    layout='c')})
+        return x
