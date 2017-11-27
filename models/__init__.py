9321e41c08f15911f22c02d719a8f63a611c7c86
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-11-27 19:17:31 +0300

    Changed imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,10 @@
 """ Module of nn-models for classification/segmentation of lung-cancer on CT-scans. """
-from .tensorflow.architectures import TFDenseNoduleNet
-from .tensorflow.architectures import TFResNoduleNet
-from .tensorflow.architectures import TFDilatedNoduleVnet
+from .tensorflow.architectures import DenseNoduleNet
+from .tensorflow.architectures import ResNoduleNet
+from .tensorflow.architectures import DilatedNoduleVNet
 from .keras.architectures import KerasNoduleVnet
 from .keras.architectures import KerasResNoduleNet
 from .keras.architectures import KerasNoduleVGG
-from .tensorflow import TFModelCT
-from .keras import KerasModel as KerasModelCT
 from .utils import unpack_seg
 from .utils import unpack_clf
 from .utils import unpack_reg
--- models/tensorflow/architectures/__init__.py
+++ models/tensorflow/architectures/__init__.py
@@ -1,4 +1,4 @@
 """ Contains neural network architectures for lung cancer detection implemented in tensorflow. """
-from .dense_net import TFDenseNoduleNet
-from .resnet import TFResNoduleNet
-from .dilated_vnet import TFDilatedNoduleVnet
+from .dense_net import DenseNoduleNet
+from .resnet import ResNoduleNet
+from .vnet import DilatedNoduleVNet
