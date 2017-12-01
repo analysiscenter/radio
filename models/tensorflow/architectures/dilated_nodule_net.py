e10d740eb783a3951b00d3ee4b01dc35db82d3f6
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-01 14:13:36 +0300

    Removed useless log message

--- models/tensorflow/architectures/dilated_nodule_net.py
+++ models/tensorflow/architectures/dilated_nodule_net.py
@@ -303,8 +303,6 @@ class DilatedNoduleNet(TFModel):
         kwargs = cls.fill_params('head', **kwargs)
         pred_kwargs = dict(filters=num_classes, kernel_size=1,
                            activation=tf.nn.sigmoid, layout='cna')
-
-        LOGGER.debug("kwargs of last conv layer in head block: {}".format(last_conv_kwargs))
         with tf.variable_scope(name):
             x = conv_block(inputs, name='conv', **kwargs)
             LOGGER.debug("Shape after first conv in head: {}".format(cls.get_shape(x)))
