c3697ff041ef91a97ce16001761b068fac18f6f3
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-01 13:13:17 +0300

    Add logger

--- models/tensorflow/architectures/dilated_nodule_net.py
+++ models/tensorflow/architectures/dilated_nodule_net.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import tensorflow as tf
+import logging
 from ..layers import bn_conv3d, bn_dilated_conv3d
 from ..utils import repeat_tensor
 from ....dataset.dataset.models.tf.layers import conv_block
