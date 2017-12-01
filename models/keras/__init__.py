2bc52fe2cca0cc520e6b8938be1e820b47704dab
Alexey Ushakov <a.ushakov@analysiscenter.ru>
2017-12-01 15:28:59 +0300

    fix import of renamed models

--- models/keras/__init__.py
+++ models/keras/__init__.py
@@ -1,4 +1,4 @@
 """ Contains losses, usefull utils and ANN architectures implemented via keras. """
 from .losses import dice_loss, tiversky_loss
 from .keras_model import KerasModel
-from .architectures import KerasNoduleVGG, KerasNoduleVNet, KerasResNoduleNet
+from .architectures import KerasNoduleVGG, Keras3DUNet, KerasResNoduleNet
