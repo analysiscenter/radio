c75ee1be89a63683dc6561bb614abd07db61c856
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-04 16:03:29 +0300

    Remove update of head block config dictionary

--- models/tensorflow/architectures/dense_nodule_net.py
+++ models/tensorflow/architectures/dense_nodule_net.py
@@ -14,7 +14,6 @@ class DenseNoduleNet(DenseNet):
         input_config = dict(layout='cnap', filters=16, kernel_size=7,
                             pool_size=3, pool_strides=(1, 2, 2))
 
-        config['head'].update(dict(layout='Vdfna', activation=tf.nn.sigmoid))
         config['input_block'].update(input_config)
         config['body']['num_blocks'] = [6, 12, 24, 16]
         return config
