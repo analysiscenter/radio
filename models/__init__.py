0f9322203b5897d69bb4a24fbf10d5647c1c7240
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-04 14:09:10 +0300

    Change model class name in import in __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 """ Module of nn-models for classification/segmentation of lung-cancer on CT-scans. """
 from .tensorflow.architectures import DenseNoduleNet
-from .tensorflow.architectures import ResNoduleNet
+from .tensorflow.architectures import ResNodule3DNet50
 from .tensorflow.architectures import DilatedNoduleNet
 from .keras.architectures import Keras3DUNet
 from .keras.architectures import KerasResNoduleNet
