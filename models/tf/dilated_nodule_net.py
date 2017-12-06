8d2d7aef8a52a9e6fc561e3ddd388709a8abac8a
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-07 01:28:58 +0300

    Add name in conv_block call

--- models/tf/dilated_nodule_net.py
+++ models/tf/dilated_nodule_net.py
@@ -90,7 +90,8 @@ class DilatedNoduleNet(TFModel):
         with tf.variable_scope(name):
             for f, k, d in zip(filters, kernel_size, dilation_rate):
 
-                b = conv_block(inputs, layout='cna', kernel_size=k, dilation=d, filters=f, **kwargs)
+                b = conv_block(inputs, layout='cna', kernel_size=k, dilation=d,
+                               filters=f, name='conv_dilation_{}'.format(d) **kwargs)
                 branches.append(b)
             outputs = tf.concat(branches, axis=4)
         return outputs
