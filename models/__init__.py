8ca736967363454974bb8dcd2f2265bebe9ee244
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-06 13:37:17 +0300

    Change __init__: tensorflow => tf in import paths

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 """ Module of nn-models for classification/segmentation of lung-cancer on CT-scans. """
-from .tensorflow.architectures import DenseNoduleNet
-from .tensorflow.architectures import ResNodule3DNet50
-from .tensorflow.architectures import DilatedNoduleNet
+from .tf.architectures import DenseNoduleNet
+from .tf.architectures import ResNodule3DNet50
+from .tf.architectures import DilatedNoduleNet
 from .keras.architectures import Keras3DUNet
 from .keras.architectures import KerasResNoduleNet
 from .keras.architectures import KerasNoduleVGG
