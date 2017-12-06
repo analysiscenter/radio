27f38f30d142b01e3cd599b6c572d61727308c90
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-07 01:10:03 +0300

    Simplify conv_block call in dilated_branches

--- models/tf/dilated_nodule_net.py
+++ models/tf/dilated_nodule_net.py
@@ -91,10 +91,7 @@ class DilatedNoduleNet(TFModel):
         with tf.variable_scope(name):
             for f, k, d in zip(filters, kernel_size, dilation_rate):
 
-                b = conv_block(inputs, layout='cna', activation=activation,
-                               name='conv_brach_dilation_{}'.format(d),
-                               kernel_size=k, dilation=d, filters=f,
-                               is_training=is_training, **kwargs)
+                b = conv_block(inputs, layout='cna', kernel_size=k, dilation=d, filters=f, **kwargs)
                 branches.append(b)
             outputs = tf.concat(branches, axis=4)
         return outputs
