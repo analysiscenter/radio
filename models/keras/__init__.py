93464f8975f10dc43427f0137dcde992ef061cb1
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2017-12-06 13:43:34 +0300

    Move ANN architectures from submodule architectures to ./keras

--- models/keras/__init__.py
+++ models/keras/__init__.py
@@ -1,4 +1,4 @@
-""" Contains losses, usefull utils and ANN architectures implemented via keras. """
-from .losses import dice_loss, tiversky_loss
-from .keras_model import KerasModel
-from .architectures import KerasNoduleVGG, Keras3DUNet, KerasResNoduleNet
+""" Contains neural network architectures for lung cancer detection implemented in keras. """
+from .keras_res_nodule_net import KerasResNoduleNet
+from .keras_3dunet import Keras3DUNet
+from .keras_nodule_vgg import KerasNoduleVGG
--- models/keras/architectures/__init__.py
+++
@@ -1,4 +0,0 @@
-""" Contains neural network architectures for lung cancer detection implemented in keras. """
-from .keras_res_nodule_net import KerasResNoduleNet
-from .keras_3dunet import Keras3DUNet
-from .keras_nodule_vgg import KerasNoduleVGG
