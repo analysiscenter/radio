4a1aa37fc988f43072d14d93f8c135b33445166d
Alexander Koryagin <a.koryagin@analysiscenter.ru>
2018-01-10 14:40:32 +0300

    update slices-plotting func: now plots several slices; can plot inverse-spacing grid

--- examples/utils.py
+++ examples/utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def show_slice(batch, scan_index, n_slice, **kwargs):
+def show_slices(batches, scan_indices, ns_slice, **kwargs):
+def show_slices(batches, scan_indices, ns_slice, **kwargs):
     """ Plot slice with number n_slice from scan with index given by scan_index from batch
     """
     font_caption = {'family': 'serif',
@@ -12,16 +14,45 @@ def show_slice(batch, scan_index, n_slice, **kwargs):
             'weight': 'normal',
             'size': 15}
 
-    slc = batch.get(scan_index, 'images')[n_slice]
-    _, axes = plt.subplots(1, 1, squeeze=False, figsize=(10, 4))
-    axes[0][0].imshow(slc, cmap=plt.cm.gray, clim=kwargs.get('clim', (-1200, 300)))
-    axes[0][0].set_xlabel('Shape: {}'.format(slc.shape[1]), fontdict=font)
-    axes[0][0].set_ylabel('Shape: {}'.format(slc.shape[0]), fontdict=font)
-    axes[0][0].set_xticks([])
-    axes[0][0].set_yticks([])
-    axes[0][0].set_title('Slice #{} \n \n'.format(n_slice), fontdict=font_caption)
-    axes[0][0].text(0.15, -0.25, 'Total slices: {}'.format(len(batch.get(scan_index, 'images'))),
-                    fontdict=font_caption,
-                    verticalalignment='center', transform=axes[0][0].transAxes)
+    # fetch some arguments, make iterables out of args
+    def iterize(arg):
+        return arg if isinstance(arg, (list, tuple)) else (arg, )
+
+    batches, scan_indices, ns_slice  = [iterize(arg) for arg in (batches, scan_indices, ns_slice)]
+    clims = kwargs.get('clims', (-1200, 300))
+    clims = clims if isinstance(clims[0], (tuple, list)) else (clims, )
+
+    # lengthen args
+    n_boxes = max(len(arg) for arg in (batches, scan_indices, ns_slice, clims))
+    def lengthen(arg):
+        return arg if len(arg) == n_boxes else arg * n_boxes
+
+    batches, scan_indices, ns_slice, clims = [lengthen(arg) for arg in (batches, scan_indices, ns_slice, clims)]
+
+    # plot slices
+    _, axes = plt.subplots(1, n_boxes, squeeze=False, figsize=(10, 4 * n_boxes))
+
+    for i, batch, scan_index, n_slice, clim in zip(range(n_boxes), batches, scan_indices, ns_slice, clims):
+        slc = batch.get(scan_index, 'images')[n_slice]
+        axes[0][i].imshow(slc, cmap=plt.cm.gray, clim=clim)
+        axes[0][i].set_xlabel('Shape: {}'.format(slc.shape[1]), fontdict=font)
+        axes[0][i].set_ylabel('Shape: {}'.format(slc.shape[0]), fontdict=font)
+        axes[0][i].set_title('Scan #{}, slice #{} \n \n'.format(i, n_slice), fontdict=font_caption)
+        axes[0][i].text(0.2, -0.25, 'Total slices: {}'.format(len(batch.get(scan_index, 'images'))),
+                        fontdict=font_caption, transform=axes[0][i].transAxes)
+
+        # set inverse-spacing grid
+        inv_spacing = 1 / batch.get(scan_index, 'spacing').reshape(-1)[1:]
+        print(inv_spacing)
+        step_mult = 50
+        xticks = np.arange(0, slc.shape[0], step_mult * inv_spacing[0])
+        yticks = np.arange(0, slc.shape[1], step_mult * inv_spacing[1])
+        axes[0][i].set_xticks(xticks, minor=True)
+        axes[0][i].set_yticks(yticks, minor=True)
+        axes[0][i].set_xticks([], minor=False)
+        axes[0][i].set_yticks([], minor=False)
+
+        axes[0][i].grid(color='r', linewidth=1.5, alpha=0.5, which='minor')
+
 
     plt.show()
