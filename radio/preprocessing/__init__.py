25cb23bea8574131584c623726ddc71416b58a01
a.kozhevin <a.kozhevin@analysiscenter.ru>
2018-02-20 17:04:36 +0300

    add functions in nodules

--- radio/preprocessing/__init__.py
+++ radio/preprocessing/__init__.py
@@ -3,3 +3,4 @@
 from .ct_batch import CTImagesBatch
 from .ct_masked_batch import CTImagesMaskedBatch
 from .histo import sample_ellipsoid_region
+from .nodules import Nodules
--- radio/preprocessing/nodules.py
+++ radio/preprocessing/nodules.py
@@ -1,41 +1,68 @@
+""" Class class for nodules. """
+
 import numpy as np
-from numba import jitclass
-from numba import int32, float32, float64
 import pandas as pd
 
-items = [
-    ('id', int32[:]),
-    ('x', float64[:]),
-    ('y', float64[:]),
-    ('z', float64[:]),
-    ('diameter', float64[:]),
-]
+COMPONENTS = {
+    'luna': ('seriesuid', 'coordX', 'coordY', 'coordZ', 'diameter_mm'),
+    'radio': ('id', 'x', 'y', 'z', 'diameter')
+}
 
 class Nodules:
-    def __init__(self, nodules):
-        nodules = {k: np.array(v) for k, v in dict(nodules).items()}
-        nodules['seriesuid'] = self._ids_to_ints(nodules['seriesuid'])
-        self._nodules_numba = NodulesNumba(**nodules)
+    """ Class for storing information about cancer nodules. """
+    def __init__(self, nodules=None, names='luna'):
+        """
+        Parameters
+        ----------
+        nodules : dict, pd.DataFrame or None
+        
+        names : str
+        """
+        if isinstance(nodules, (pd.DataFrame, dict)):
+            nodules = {k: np.array(v) for k, v in dict(nodules).items()}
+            self._components = self._build_nodules(nodules, names)
+        elif type(self) == type(nodules):
+            self._components = nodules._components
+        elif nodules is None:
+            self._components = {key: [] for key in COMPONENTS['radio']}
 
-    @property
-    def x(self):
-        return self.nodules.x
-
-    @property
-    def y(self):
-        return self.nodules.y
-
-    @property
-    def z(self):
-        return self.nodules.z
+    def _build_nodules(self, nodules, names):
+        components = {key1: nodules[key2] for key1, key2 in zip(COMPONENTS['radio'], COMPONENTS[names])}
+        return components
 
     @property
     def id(self):
-        return self._ints_to_ids(self.nodules.id)
+        return self._components['id']
 
     @property
-    def diameter(self):
-        return self.nodules.diameter
+    def x(self):
+        return self._components['x']
+
+    @property
+    def y(self):
+        return self._components['y']
+
+    @property
+    def z(self):
+        return self._components['z']
+
+    @property
+    def diameter_mm(self):
+        return self._components['diameter']
+
+    @property
+    def center(self):
+        return np.stack([self.x, self.y, self.z], axis=1)
+
+    @property
+    def size(self):
+        return np.stack([self.diameter_mm] * 3, axis=1)
+
+    def set_ids(self, new_ids):
+        if len(new_ids) != len(self):
+            raise ValueError("'new_ids' must be of length {} but length is {}"
+                .format(len(self), len(new_ids)))
+        self._components['id'] = np.asarray(new_ids)
 
     def _ids_to_ints(self, ids):
         self.mapping = {value: key for key, value in enumerate(np.unique(ids))}
@@ -45,29 +72,81 @@ class Nodules:
         reverse_mapping = {value: key for key, value in self.mapping.items()}
         return np.array(list(map(reverse_mapping.get, ids)))
 
-    def get_nodules(self):
+    def select(self, _sentinel=None, ids=None, batch=None, indices=None, coords=None, as_df=False):
+        """ Select nodules that corresponds to the batch or ids. """
+        if _sentinel is not None:
+            raise ValueError("Only call `select` with named arguments.")
+        if ids is not None:
+            _indices = np.in1d(self.id, ids)
+        elif batch is not None:
+            _indices = np.in1d(self.id, batch.index.indices)
+        elif indices is not None:
+            return {key: value[indices] for key, value in self._components.items()}
+        else:
+            raise ValueError("At least one of 'indices' and 'batch' must be not None")
 
+        components = {key: value[_indices] for key, value in self._components.items()}
+        if coords is not None:
+            components = self._filter_by_coords(components, coords)
+        if as_df:
+            components = pd.DataFrame(components).set_index('id')
+        return components
 
+    def filter(self, _sentinel=None, ids=None, batch=None, indices=None, coords=None):
+        """ Filter nodules that corresponds to the batch or indices. """
+        if _sentinel is not None:
+            raise ValueError("Only call `filter` with named arguments.")
+        self._components = self.select(ids=ids, batch=batch, indices=indices, coords=coords)
 
-@jitclass(items)
-class NodulesNumba:
-    def __init__(self, seriesuid, coordX, coordY, coordZ, diameter_mm):
-        self._build_nodules(seriesuid, coordX, coordY, coordZ, diameter_mm)
+    def batch_indices(self, batch):
+        """ Indices of patients in batch that has nodules. """
+        _indices = np.in1d(self.id, batch.index.indices)
+        _indices = self.id[_indices]
+        return batch.index.get_pos(_indices)
 
-    def _build_nodules(self, patient_id, x, y, z, diameter):
-        self.id = patient_id
-        self.x = x
-        self.y = y
-        self.z = z
-        self.diameter = diameter
+    def extend(self, nodules):
+        """ Extend nodules. """
+        self._components = {
+            key: np.concatenate([getattr(self, key), getattr(nodules, key)], axis=0)
+            for key in self._components
+        }
 
-    def get_nodules(self, patient_id):
-        indices = [i for i in len(self.id) if patient_id == self.id[i]]
-        nodules = NodulesNumba(
-            self.id[indices],
-            self.x[indices],
-            self.y[indices],
-            self.z[indices],
-            self.diameter[indices]
-        )
-        return nodules
+    def __len__(self):
+        return len(self.id)
+
+    @classmethod
+    def assemble(self, *nodules):
+        """ Concatenate nodules. """
+        new_nodules = Nodules()
+        [new_nodules.extend(item) for item in nodules]
+        return new_nodules
+
+    def random_sample(self, size):
+        """ Select random nodules. """
+        indices = np.random.choice(len(self), size, replace=False)
+        return self.select(indices=indices)
+
+    def spacing(self, batch):
+        """ Get spacing for nodules in batch. """
+        return batch.data.spacing[batch.index.get_pos(self.id)]
+
+    def origin(self, batch):
+        """ Get origin for nodules in batch. """
+        return batch.data.origin[batch.index.get_pos(self.id)]
+
+    def patient_pos(self, batch):
+        """ Get patients' indices in batch. """
+        return batch.index.get_pos(self.id)
+
+    def center_in_pixels(self, batch):
+        """ Get coords of nodules' centers in images. """
+        return np.abs(self.center - self.origin(batch)) / self.spacing(batch)
+    
+    def start_in_pixels(self, batch):
+        """ Get nodules' starts in images. """
+        start_pix = (self.center_in_pixels(batch) - self.size_in_pixels(batch) / 2)
+        return np.rint(start_pix).astype(np.int)
+
+    def size_in_pixels(self, batch):
+        """ Get nodules sizes. """
+        return np.rint(self.size / self.spacing(batch))
