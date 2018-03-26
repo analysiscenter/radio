831ede23b8f1b9637f4e652f3c444f28e4147226
Alexey Ushakov <kafka.pochta@gmail.com>
2018-03-26 19:25:55 +0300

    handle confidence, make int prediction for connected components

--- radio/preprocessing/ct_masked_batch.py
+++ radio/preprocessing/ct_masked_batch.py
@@ -113,7 +113,8 @@ class CTImagesMaskedBatch(CTImagesBatch):
                               ('nodule_center', np.float, (3,)),
                               ('nodule_size', np.float, (3,)),
                               ('spacing', np.float, (3,)),
-                              ('origin', np.float, (3,))])
+                              ('origin', np.float, (3,)),
+                              ('confidence', np.float, (1,))])
 
     components = "images", "masks", "spacing", "origin"
 
@@ -174,7 +175,7 @@ class CTImagesMaskedBatch(CTImagesBatch):
             centers, ids and sizes of nodules.
         """
         columns = ['nodule_id', 'source_id', 'locZ', 'locY',
-                   'locX', 'diamZ', 'diamY', 'diamX']
+                   'locX', 'diamZ', 'diamY', 'diamX', 'confidence']
 
         nodule_id = self.make_indices(nodules.shape[0])
         return pd.DataFrame({'source_id': self.indices[nodules.patient_pos],
@@ -184,7 +185,8 @@ class CTImagesMaskedBatch(CTImagesBatch):
                              'locX': nodules.nodule_center[:, 2],
                              'diamZ': nodules.nodule_size[:, 0],
                              'diamY': nodules.nodule_size[:, 1],
-                             'diamX': nodules.nodule_size[:, 2]}, columns=columns)
+                             'diamX': nodules.nodule_size[:, 2],
+                             'confidence': nodules.confidence[:,0]}, columns=columns)
 
     def get_pos(self, data, component, index):
         """ Return a positon of an item for a given index in data
@@ -359,9 +361,9 @@ class CTImagesMaskedBatch(CTImagesBatch):
         """
         nodules_list = []
         for pos in range(len(self)):
-            mask = self.get(pos, 'masks')
+            mask = self.get(pos, 'masks') * 2
             mask_labels = measure.label(mask, background=0)
-            for props in measure.regionprops(np.int16(mask_labels)):
+            for props in measure.regionprops(mask_labels, intensity_image=mask):
                 center = np.asarray((props.centroid[0],
                                      props.centroid[1],
                                      props.centroid[2]), dtype=np.float)
@@ -370,9 +372,11 @@ class CTImagesMaskedBatch(CTImagesBatch):
                 diameter = np.asarray(
                     [props.equivalent_diameter] * 3, dtype=np.float)
                 diameter = diameter * self.spacing[pos]
+                confidence = props.mean_intensity / 2
                 nodules_list.append({'patient_pos': pos,
                                      'nodule_center': center,
-                                     'nodule_size': diameter})
+                                     'nodule_size': diameter,
+                                     'confidence': confidence})
 
         num_nodules = len(nodules_list)
         self.nodules = np.rec.array(
@@ -381,6 +385,8 @@ class CTImagesMaskedBatch(CTImagesBatch):
             self.nodules.patient_pos[i] = nodule['patient_pos']
             self.nodules.nodule_center[i, :] = nodule['nodule_center']
             self.nodules.nodule_size[i, :] = nodule['nodule_size']
+            self.nodules.confidence[i] = nodule['confidence']
+
         self._refresh_nodules_info(images_loaded)
         return self
 
