51ad44d8e60a497818334b36a60d5840e88f2777
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2018-04-09 23:16:43 +0300

    Correct name of source file in import

--- radio/annotation/__init__.py
+++ radio/annotation/__init__.py
@@ -3,4 +3,4 @@
 from .parser import read_nodules, read_dataset_info
 from .nodules_merger import assign_nodules_group_index, get_nodules_groups
 from .nodules_merger import compute_group_coords_and_diameter
-from .doctor_confidences import compute_confidences as get_doctors_confidences
+from .doctor_confidence import compute_confidences as get_doctors_confidences
