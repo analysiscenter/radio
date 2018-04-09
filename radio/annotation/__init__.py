abb1ab112a16cc744738712625f06ccc44964d3f
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2018-04-09 23:11:10 +0300

    Add __init__.py

---
+++ radio/annotation/__init__.py
@@ -0,0 +1,6 @@
+""" Functions for processing annotation provided for dicom dataset. """
+
+from .parser import read_nodules, read_dataset_info
+from .nodules_merger import assign_nodules_group_index, get_nodules_groups
+from .nodules_merger import compute_group_coords_and_diameter
+from .doctor_confidences import compute_confidences as get_doctors_confidences
