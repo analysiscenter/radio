62c15e7ab1b0c7569e5aa543522c5f16ecf9e5f9
Alexander Koryagin <a.koryagin@analysiscenter.ru>
2018-04-10 18:01:17 +0300

    simplify cnodule-confidence computation function; add docstring

--- radio/annotation/nodule_confidence.py
+++ radio/annotation/nodule_confidence.py
@@ -2,48 +2,70 @@ import pandas as pd
 import numpy as np
 
 def ep(u):
-    """ vectorized Epanechnikov kernel """
+    """ Vectorized Epanechnikov kernel.
+
+    Parameters
+    ----------
+    u : ndarray
+        input array of distances.
+
+    Return
+    ------
+    ndarray
+        array of ep(input array-items).
+    """
     return 0.75 * (1 - u**2) * (np.abs(u) <= 1).astype(np.float)
 
-def compute_nodule_confidence(annotations, scan_id, nodule_id=None, r=20, alpha=None,
-                              weight_by_doctor=True):
-    scan = annotations[annotations.AccessionNumber==scan_id]
-    nodule_id = scan.NoduleID.unique() if nodule_id is None else nodule_id
+def compute_nodule_confidence(annotations, r=20, alpha=None, weight_by_doctor=True):
+    """ Compute nodule confidence using annotations-df; put confidences into a new column
+    'NoduleConfidence'.
 
-    # iterize nodule_id
-    nodule_id = np.array(nodule_id).reshape(-1)
+    Parameters
+    ----------
+    annotations : pd.DataFrame
+        input df with annotations.
+    r : float
+        radius of kernel-support.
+    alpha : float or None
+        weight of target-nodule's doctor in weighted sum.
+    weight_by_doctor : bool
+        whether the weighted sum should be weighted by target-nodule's doctor.
 
-    # matrix of distances
-    scan_cleaned = scan.drop(labels=['annotator_1', 'annotator_2', 'annotator_3',
-                             'diameter_mm', 'NoduleType', 'AccessionNumber'], axis=1)
-    scan_cleaned['_key'] = 0
-    pairwise = (pd
-                .merge(scan_cleaned, scan_cleaned, how='inner', left_on='_key', right_on='_key', suffixes=('', '_other'))
-                .drop(labels=['_key'], axis=1))
+    Return
+    ------
+    pd.DataFrame
+        annotations-dataframe with added 'NoduleConfidence'-column.
+    """
+    # matrix of distances between nodules from the same scan
+    cleaned = annotations.copy().drop(labels=['annotator_1', 'annotator_2', 'annotator_3',
+                                              'diameter_mm', 'NoduleType'], axis=1)
+    pairwise = pd.merge(cleaned, cleaned, how='inner', left_on='AccessionNumber',
+                        right_on='AccessionNumber', suffixes=('', '_other'))
     pairwise['Distance'] = np.sqrt((pairwise.coordX - pairwise.coordX_other)**2 + (pairwise.coordY - pairwise.coordY_other)**2 +
                                    (pairwise.coordZ - pairwise.coordZ_other)**2)
     pairwise = pairwise.drop(labels=(['coord' + capital for capital in ['X', 'Y', 'Z']] +
                                      ['coord' + capital + '_other' for capital in ['X', 'Y', 'Z']]), axis=1)
 
     # compute kernel-weights of nodules
-    nodule_ds = pairwise[[x in nodule_id for x in pairwise.NoduleID]]
-    nodule_in_support = nodule_ds[nodule_ds.Distance <= r]
-    nodule_in_support['weights'] = ep(nodule_in_support.Distance / r)
-    nodule_in_support['weights'] *= np.maximum(nodule_in_support.DoctorID_other!=nodule_in_support.DoctorID,
-                                               nodule_in_support.NoduleID_other==nodule_in_support.NoduleID)
+    pairwise = pairwise[pairwise.Distance <= r]
+    pairwise['weights'] = ep(pairwise.Distance / r)
+    pairwise['weights'] *= np.maximum(pairwise.DoctorID_other!=pairwise.DoctorID,
+                                      pairwise.NoduleID_other==pairwise.NoduleID)
 
     # compute confidences
-    nodule_in_support['weighted_confs'] = (nodule_in_support['weights'] * nodule_in_support['DoctorConfidence_other'])
+    pairwise['weighted_confs'] = (pairwise['weights'] * pairwise['DoctorConfidence_other'])
 
     # add doctor weights if needed
     if weight_by_doctor:
-        nodule_in_support['weighted_confs'] *= nodule_in_support['DoctorConfidence']
+        pairwise['weighted_confs'] *= pairwise['DoctorConfidence']
 
     # add correction for alpha-confidences if needed
     if alpha is not None:
         # get rid of kernel weights before target-nodules and weight by alpha, 1 - alpha
-        nodule_in_support.loc[nodule_in_support.NoduleID_other==nodule_in_support.NoduleID, 'weighted_confs'] *= alpha / ep(0)
-        nodule_in_support.loc[nodule_in_support.NoduleID_other!=nodule_in_support.NoduleID, 'weighted_confs'] *= 1 - alpha
+        pairwise.loc[pairwise.NoduleID_other==pairwise.NoduleID, 'weighted_confs'] *= alpha / ep(0)
+        pairwise.loc[pairwise.NoduleID_other!=pairwise.NoduleID, 'weighted_confs'] *= 1 - alpha
 
-    confs = nodule_in_support.groupby('NoduleID').weighted_confs.sum()
-    return confs
+    confs = pairwise.groupby('NoduleID').weighted_confs.sum()
+    confs = pd.DataFrame(confs)
+    confs.rename({'weighted_confs': 'NoduleConfidence'}, axis=1, inplace=True)
+    return pd.merge(annotations, confs, left_on='NoduleID', right_index=True)
