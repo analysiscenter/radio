d5e17b6d989bb6f58d09ac1e38260490759a8c7a
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2018-04-11 22:25:20 +0300

    Remove njit functions

--- radio/annotation/nodule_confidence.py
+++ radio/annotation/nodule_confidence.py
@@ -22,41 +22,6 @@ def ep(u):
     return 0.75 * (1 - u**2) * (np.abs(u) <= 1).astype(np.float)
 
 
-@njit
-def epanechnikov_kernel_numba(x):
-    return 0.75 * (1 - u ** 2) * (x <= 1).astype(np.float)
-
-
-@njit
-def get_distance_matrix_numba(coords):
-    num_items = coords.shape[0]
-    distance_matrix = np.zeros(shape=(num_items, num_items))
-    for i in range(num_items):
-        for j in range(num_items):
-            distance_matrix[i, j] = np.sqrt(np.sum((coords[i, :] - coords[j, :]) ** 2))
-    return distance_matrix
-
-@njit
-def get_nodules_doctors_mask_numba(nodules_doctors):
-    num_items = nodules_doctors.shape[0]
-    confidence_matrix = np.ones(shape=(num_items, num_itemss), dtype=int)
-    for i in range(num_items):
-        for j in range(num_items):
-            if i != j:
-                confidence_matrix[i, j] = int(not nodules_doctors[i] == nodules_doctors[j])
-    return confidence_matrix
-
-
-def compute_nodule_confidence(nodules, r=20, alpha=None, weight_by_doctor=True):
-    coords = nodules.loc[:, ['coordZ', 'coordY', 'coordX']].values
-    doctors_confidences = nodules.loc[:, 'DoctorConfidence'].values
-    nodules_doctors = nodules.loc[:, 'DoctorID'].astype(int).values
-
-    distance_matrix = epanechnikov_kernel_numba(get_distance_matrix_numba(coords))
-    confidences = distance_matrix * get_nodules_doctors_mask_numba(nodules_doctors)
-
-
-
 def compute_nodule_confidence(annotations, r=20, alpha=None, weight_by_doctor=True):
     """ Compute nodule confidence using annotations-df; put confidences into a new column
     'NoduleConfidence'.
