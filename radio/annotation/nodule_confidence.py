4f87aede1d7e21d52815ec75b280b774e148788e
Kirill Emelyanov <k.emelyanov@analysiscenter.ru>
2018-04-11 21:39:14 +0300

    Remove drop cause it would be better to get required columns

--- radio/annotation/nodule_confidence.py
+++ radio/annotation/nodule_confidence.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import numpy as np
+from numba import njit
+
 
 
 def ep(u):
@@ -18,6 +20,41 @@ def ep(u):
         array of ep(input array-items).
     """
     return 0.75 * (1 - u**2) * (np.abs(u) <= 1).astype(np.float)
+
+
+@njit
+def epanechnikov_kernel_numba(x):
+    return 0.75 * (1 - u ** 2) * (x <= 1).astype(np.float)
+
+
+@njit
+def get_distance_matrix_numba(coords):
+    num_items = coords.shape[0]
+    distance_matrix = np.zeros(shape=(num_items, num_items))
+    for i in range(num_items):
+        for j in range(num_items):
+            distance_matrix[i, j] = np.sqrt(np.sum((coords[i, :] - coords[j, :]) ** 2))
+    return distance_matrix
+
+@njit
+def get_nodules_doctors_mask_numba(nodules_doctors):
+    num_items = nodules_doctors.shape[0]
+    confidence_matrix = np.ones(shape=(num_items, num_itemss), dtype=int)
+    for i in range(num_items):
+        for j in range(num_items):
+            if i != j:
+                confidence_matrix[i, j] = int(not nodules_doctors[i] == nodules_doctors[j])
+    return confidence_matrix
+
+
+def compute_nodule_confidence(nodules, r=20, alpha=None, weight_by_doctor=True):
+    coords = nodules.loc[:, ['coordZ', 'coordY', 'coordX']].values
+    doctors_confidences = nodules.loc[:, 'DoctorConfidence'].values
+    nodules_doctors = nodules.loc[:, 'DoctorID'].astype(int).values
+
+    distance_matrix = epanechnikov_kernel_numba(get_distance_matrix_numba(coords))
+    confidences = distance_matrix * get_nodules_doctors_mask_numba(nodules_doctors)
+
 
 
 def compute_nodule_confidence(annotations, r=20, alpha=None, weight_by_doctor=True):
@@ -41,8 +78,8 @@ def compute_nodule_confidence(annotations, r=20, alpha=None, weight_by_doctor=Tr
         annotations-dataframe with added 'NoduleConfidence'-column.
     """
     # matrix of distances between nodules from the same scan
-    cleaned = annotations.copy().drop(labels=['annotator_1', 'annotator_2', 'annotator_3',
-                                              'diameter_mm', 'NoduleType'], axis=1)
+    cleaned = annotations.loc[:, ['coordZ', 'coordY', 'coordX', 'AccessionNumber', 'NoduleID']]
+
     pairwise = pd.merge(cleaned, cleaned, how='inner', left_on='AccessionNumber',
                         right_on='AccessionNumber', suffixes=('', '_other'))
 
