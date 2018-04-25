38df43d6f935a4dbf69d2eb3170bdff742e3c704
alexeykozhevin <akozhevin@analysiscenter.ru>
2018-04-25 13:16:37 +0000

    updated metrics

--- radio/models/cancer_metrics.py
+++ radio/models/cancer_metrics.py
@@ -1,63 +1,122 @@
+"""cancer metrics"""
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 from tqdm import tqdm_notebook as tqn
-from radio.models.utils import overlap_nodules
+from scipy.ndimage.measurements import label
+
 
 def binarization(arr, threshold=.5, upper=1, lower=0):
-    return np.where(arr > threshold, upper, lower)
+    """binarizate arr by theshold"""
+    if isinstance(arr, list):
+        arr = np.array(arr)
+    return np.where(arr > threshold, upper, lower).astype(np.uint8)
 
-def calcualte_metrics(pipeline, **kwargs):
-    true_stats = []
-    pred_stats = []
-    true_mask = []
-    pred_mask = []
-    for i, batch in tqn(enumerate(pipeline.gen_batch(batch_size=1))):
-        nodules_true = pipeline.get_variable('nodules_true')
-        nodules_pred = pipeline.get_variable('nodules_pred')
-        pred_mask.append(pipeline.get_variable('predictions'))
-        true_mask.append(pipeline.get_variable('targets'))
-        result = overlap_nodules(batch, nodules_true, nodules_pred)
-        true_stats.append(result['true_stats'])
-        pred_stats.append(result['pred_stats'])
+def get_connected_ixs(array):
+    """find connected components in the array"""
+    connected_array = label(array)
+    if connected_array[1] == 0:
+        return None
+    ixs = [np.concatenate(np.where(connected_array[0] == i)).reshape(4, -1) for i in range(1, connected_array[1]+1)]
+    return ixs
 
-    true_mask_bin = binarization(np.array(true_mask))
-    pred_mask_bin = binarization(np.array(pred_mask))
+def sensitivity(target, prediction, threshold=.5, iot=.5, **kwargs):
+    """True positive rate"""
+    target, prediction = binarization([target, prediction], threshold)
+    connected_target = get_connected_ixs(target)
+    connected_prediction = get_connected_ixs(prediction)
+    if connected_target is None:
+        if len(connected_prediction) > 0:
+            return 0
+        return 1
+    intersection = prediction * target
+    right = 0
+    for coord_nodule in connected_target:
+        mass = [slice(np.min(coord_nodule[i]), np.max(coord_nodule[i]))
+                if np.min(coord_nodule[i]) != np.max(coord_nodule[i])
+                else slice(np.min(coord_nodule[i]), np.max(coord_nodule[i])+1)
+                for i in range(coord_nodule.shape[0])]
 
-    true = pd.DataFrame(columns=['diam', 'locZ', 'locY', 'locX', 'overlap_index', 'source_id'])
-    pred = pd.DataFrame(columns=['diam', 'locZ', 'locY', 'locX', 'overlap_index', 'source_id'])
+        nodule_perc = intersection[mass]
+        if np.sum(nodule_perc)/len(nodule_perc) > iot:
+            right += 1
+    total = right / len(connected_target)
+    return total
 
-    for i, tr in enumerate(true_stats):
-        for j in tr:
-            true = true.append(j)
-        for k in pred_stats[i]:
-            pred = pred.append(k)
-    metrics = {}
-    metrics['sensitivity'] = sensitivity(true, pred, **kwargs)
-    metrics['specificity'] = specificity(true_mask_bin, pred_mask_bin, **kwargs)
-    return metrics
+def false_positive_per_scan(target, prediction, threshold=.5, iot=.5, **kwargs):
+    """number of falsely predicted nodules on scan"""
+    target, prediction = binarization([target, prediction], threshold)
+    connected_target = get_connected_ixs(target)
+    connected_prediction = get_connected_ixs(prediction)
 
-def sensitivity(true_stats, pred_stats, threshold=.5, d_min=.5, **kwargs):
-    predicted_nodules = 0
-    for ix in true_stats.iterrows():
-        true_diam, true_x, true_y, true_z  = ix[1][true_stats.columns[:4]]
-        try:
-            pred_diam, pred_x, pred_y, pred_z = pred_stats.loc[ix[1]['overlap_index']][:4]
-        except TypeError:
-            pass
-        dist = (np.abs(pred_x - true_x)**2 + np.abs(pred_y - true_y) + np.abs(pred_z - true_z)**2)**.5
-        r = pred_diam / 2
-        R = true_diam / 2
-        V = (np.pi * (R + r - dist)**2 * (dist**2 + 2*dist*r - 3*r*r + 2*dist*R + 6*r*R - 3*R*2)) / (12*dist)
-        V_true = 4/3 * np.pi * R**3
-        if V/V_true > d_min:
-            predicted_nodules +=1
-    return predicted_nodules/len(true_stats)
+    if connected_target is None:
+        if len(connected_prediction) > 0:
+            return 0
+        return 1
 
-def specificity_2(true_mask, pred_mask, theshold=0.5, **kwargs):
-    tnr = []
-    for pred, target in zip(true_mask, pred_mask):
-        tnr.append(np.sum((1 - target) * (1 - pred))/np.sum(1 - target))
-    return np.mean(tnr)
+    intersection = prediction * target
+    wrong_nodules = len(connected_prediction)
 
-def specificity(true_mask, pred_mask, **kwargs):
-    return np.sum((1-true_mask) * (1 - pred_mask)) / np.sum(1 - true_mask)
+    for coord_nodule in connected_target:
+        mass = [slice(np.min(coord_nodule[i]), np.max(coord_nodule[i]))
+                if np.min(coord_nodule[i]) != np.max(coord_nodule[i])
+                else slice(np.min(coord_nodule[i]), np.max(coord_nodule[i])+1)
+                for i in range(coord_nodule.shape[0])]
+
+        nodule_perc = intersection[mass]
+        if np.sum(nodule_perc)/len(nodule_perc) > iot:
+            wrong_nodules -= 1
+    return wrong_nodules
+
+def specificity(target, prediction, threshold=.5, **kwargs):
+    """True negative rate"""
+    target, prediction = binarization([target, prediction], threshold)
+    total = np.sum((1 - target) * (1 - prediction)) / np.sum(1 - target)
+    return total
+
+def false_discovery_rate(target, prediction, threshold=.5, **kwargs):
+    """false_discovery_rate """
+    target, prediction = binarization([target, prediction], threshold)
+    total = np.sum((1 - target) * prediction)/np.sum(prediction)
+    return total
+
+def posivite_likelihood_rato(target, prediction, threshold=.5, iot=.5, **kwargs):
+    """posivite_likelihood_rato """
+    sens = sensitivity(target, prediction, threshold, iot)
+    spec = specificity(target, prediction, threshold)
+    rato = (sens) / (1 - spec)
+    return rato
+
+def negative_likelihood_ratio(target, prediction, threshold=.5, iot=.5, **kwargs):
+    """negative_likelihood_ratio """
+    sens = sensitivity(target, prediction, threshold, iot)
+    spec = specificity(target, prediction, threshold)
+    rato = (spec) / (1 - sens)
+    return rato
+
+def froc(target, prediction, threshold=.5, iot=.5, **kwargs):
+    """froc """
+    target, prediction = binarization([target, prediction], threshold)
+    sens = []
+    false_pos = []
+    for thr in tqn(np.linspace(0, 1)):
+        sens.append(sensitivity(target, prediction, threshold=thr, iot=iot))
+        false_pos.append(false_positive_per_scan(target, prediction, threshold=thr, iot=iot))
+    plt.plot(false_pos, sens)
+    plt.show()
+
+METRICS = {
+    'sensitivity': sensitivity,
+    'false_positive_per_scan': false_positive_per_scan,
+    'specificity': specificity,
+    'false_discovery_rate': false_discovery_rate,
+    'posivite_likelihood_rato': posivite_likelihood_rato,
+    'negative_likelihood_ratio': negative_likelihood_ratio,
+    'froc': froc
+}
+
+def calcualte_metrics(target, prediction, metrics, **kwargs):
+    """calculated metrics by given metrics"""
+    calculated_metrics = {}
+    for metric in metrics:
+        calculated_metrics[metric] = METRICS[metric](target, prediction, **kwargs)
+    return calculated_metrics
