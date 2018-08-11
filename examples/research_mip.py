163cdd8f607b94e99103778404ef1adc318d79bc
Alexander Koryagin <a.koryagin@analysiscenter.ru>
2018-08-12 01:24:49 +0300

    update and move example-research

--- examples/research_mip.py
+++ examples/research_mip.py
@@ -14,22 +14,25 @@ sys.path.append('../../')
 from radio import CTImagesMaskedBatch as CTIMB
 from radio.dataset import Dataset, Pipeline, FilesIndex, F, V, B, C, Config, L
 from radio.dataset.research import Research, Option, KV
-from radio.dataset.models.tf import UNet, VNet, GCN
+from radio.dataset.models.tf import UNet, VNet
 from radio.dataset.models.tf.losses import dice_batch, dice, dice2, dice_batch, dice_batch2
 
-# npcmr in blosc
-PATH_NPCMR_SCANS = '/notebooks/ct/npcmr_blosc/*'
-PATH_NPCMR_ANNOTS = '/notebooks/ct/annotations/merged_nodules.pkl'
-index = FilesIndex(path=PATH_NPCMR_SCANS, dirs=True, no_ext=False)
+# paths to scans in blosc and annotation
+PATH_SCANS = 'blosc/*'
+PATH_ANNOTS = 'annotations.csv'
+
+# dataset and annotations-table
+index = FilesIndex(path=PATH_SCANS, dirs=True, no_ext=False)
 dataset = Dataset(index=index, batch_class=CTIMB)
 dataset.split(0.9, shuffle=120)
-dataset.indices.shape
+nodules = pd.read_csv(PATH_ANNOTS)
 
-# custom train method
 def train(batch, model='net', minibatch_size=8, mode='max', depth=6, stride=2, start=0, channels=3):
+    """ Custom train method. Train a model in minibatches of xips, fetch loss and prediction.
+    """
     # training components
-    batch.nimages = batch.xip_component('images', mode, depth, stride, start, channels)
-    batch.nmasks = batch.xip_component('masks', 'max', depth, stride, start, channels, squeeze=True)
+    batch.nimages = batch.xip('images', mode, depth, stride, start, channels)
+    batch.nmasks = batch.xip('masks', 'max', depth, stride, start, channels, squeeze=True)
 
     # train model on minibatches, fetch predictions
     model = batch.get_model_by_name(model)
@@ -50,8 +53,9 @@ def train(batch, model='net', minibatch_size=8, mode='max', depth=6, stride=2, s
     batch.pipeline.set_variable('loss', loss)
 
 
-# custom loss: rebalanced logloss with sigmoids
 def logloss(labels, predictions, coeff=10, e=1e-15):
+    """ Weighted logloss.
+    """
     predictions = tf.sigmoid(predictions)
     predictions = predictions + e       # add separation from zero
     loss = coeff * labels * tf.log(predictions) + (1 - labels) * tf.log(1 - predictions)
@@ -60,29 +64,20 @@ def logloss(labels, predictions, coeff=10, e=1e-15):
     return loss
 
 
-# function for saving model
 def save_model(batch, pipeline, model='net'):
+    """ Function for saving model.
+    """
     model = pipeline.get_model_by_name(model)
     #name = pipeline.config['model_config/loss'].__name__
     name = model.__class__.__name__
     model.save('MIP_e_6_3c/models/' + name)
     print('Model', name, 'saved')
 
-# read df with confidenced nodules info
-with open(PATH_NPCMR_ANNOTS, 'rb') as file:
-    confidenced = pkl.load(file)
-
-# nodules filtering function
-def filter_nodules(confidenced, threshold=0.02):
-    return confidenced[confidenced.confidence > threshold]
-
 # root, train, test pipelines
 root_pipeline = (
     Pipeline()
       .load(fmt='blosc', components=['images', 'spacing', 'origin'])
-      .init_variable('filtered')
-      .update_variable('filtered', L(filter_nodules, confidenced=confidenced))
-      .fetch_nodules_info(nodules=V('filtered'))
+      .fetch_nodules_info(nodules=nodules)
       .create_mask()
       .run(batch_size=4, shuffle=True, n_epochs=None, prefetch=3, lazy=True)
 )
@@ -107,12 +102,9 @@ test_pipeline = (
 )
 
 
-# # List available GPUs
-gpus = [dict(model_config=dict(device='/device:GPU:' + str(i))) for i in range(2)]
+# define research plan
 
-# # Define research plan
-
-# ## Model config
+# model config
 model_config = dict(
     inputs=dict(
         images=dict(shape=(256, 256, 3)),
@@ -127,21 +119,17 @@ model_config = dict(
 )
 
 
-# ## Pipeline config
+# pipeline config
 pipeline_config = Config(model_config=model_config)
 train_pipeline.set_config(pipeline_config)
 
 
-# ## Specify options
-#p1 = KV('model_config/loss', 'loss')
-#op = Option(p1, [logloss, dice, dice2])
-
+# specify options
 p1 = KV('model', 'model')
-op = Option(p1, [UNet, VNet]) #, GCN])
+op = Option(p1, [UNet, VNet])
 
 
-# ## Define research
-
+# define research
 mr = Research()
 mr.add_pipeline(root_pipeline << dataset.train, train_pipeline, variables=['loss'], name='train')
 mr.add_pipeline(root_pipeline << dataset.test, test_pipeline, variables=['loss'], name='test',
@@ -149,5 +137,5 @@ mr.add_pipeline(root_pipeline << dataset.test, test_pipeline, variables=['loss']
 mr.add_grid_config(op)
 
 
-# # Run research
-mr.run(n_reps=1, n_iters=50000, n_workers=1, n_branches=gpus, name='MIP_e_6_3c')
+# run research
+mr.run(n_reps=1, n_iters=50000, n_workers=1, n_branches=gpus, name='MIP_research')
